2db2bca2aa210d08f9fbbbe2a7eb9f387cce27b1
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2023-11-29 02:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (5.56%) - LeetSync

--- 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     string freqAlphabets(string s) {
+        int i=0;
         string ans="";
-        
-        int j=s.size()-1;
-        while( j>=0 )
-        {
-            if(s[j]!='#')
-            {
-                int temp=s[j--]-'0';
-                ans=ans + (char)( (temp-1) + 'a') ;
-                
-            }
+        stack< int > st;
 
+        while(i<s.length())
+        {
+            if(s[i]=='#')
+            {
+                int x1=st.top();
+                st.pop();
+                int x2=st.top();
+                st.pop();
+
+                int res= x2*10 + x1;
+                //pushing the result again into stack 
+                st.push(res);
+            }
             else
             {
-                int temp=stoi(s.substr( j-2, 2));
-                ans=ans + (char)( (temp-1) +'a');
-                j-=3;
+                st.push( s[i] - '0' );
             }
+            i++;
+        }
+
+        
+        //now generating an ans string with all the answers in reverse order
+
+        while(!st.empty())
+        {
+            ans= ans + (char)( st.top()+'a' -1 );
+            st.pop();
         }
         reverse(ans.begin(),ans.end());
         return ans;
