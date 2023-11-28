c69650ab835f24a8dbbe8be693e6be7be5c05b7a
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2023-11-29 00:19:30 +0530

    Time: 19 ms (48.29%) | Memory: 19.6 MB (36.94%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-      int i=0,count=0;
-      while(count < nums.size())
-      {
-          if(nums[i]==0)
-          {
-              nums.push_back(0);
-              nums.erase(nums.begin()+i);
-          }
-          else i++;
-          count++;
-          
-      }
-       
+        //perfect beautiful solution in my opinion
+
+        int i=0;
+        for(int curr : nums )
+        {
+            //if curr is non zero then we are plaing it at its correct
+            //relative position in vector using i
+
+            if(curr != 0)
+            {
+                nums[i]=curr;
+                i++;
+            }
+        }
+
+        //once relative positioinng of all non zero done 
+        //initialising rest with 0
+
+        while(i<nums.size())
+        {
+            nums[i]=0;
+            i++;
+        }
     }
 };
