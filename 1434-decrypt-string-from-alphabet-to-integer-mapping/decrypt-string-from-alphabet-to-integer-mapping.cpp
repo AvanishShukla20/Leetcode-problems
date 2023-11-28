83b35ff667e5047f84af056916622b4fabd854d9
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2023-11-29 01:11:32 +0530

    Time: 6 ms (8.94%) | Memory: 8.9 MB (5.56%) - LeetSync

--- 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -8,22 +8,15 @@ public:
         {
             if(s[j]!='#')
             {
-                cout<<"s[j] : "<<s[j]<<endl;
-                cout<<"s[j]+a : "<<s[j]+'a'<<endl;
-                int x='a';
-                cout<<x<<endl;
-
                 int temp=s[j--]-'0';
-
                 ans=ans + (char)( (temp-1) + 'a') ;
-                cout<<"ans :"<<ans<<endl;
+                
             }
 
             else
             {
                 int temp=stoi(s.substr( j-2, 2));
                 ans=ans + (char)( (temp-1) +'a');
-                cout<<"ans :"<<ans<<endl;
                 j-=3;
             }
         }
