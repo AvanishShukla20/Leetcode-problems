46b442813d4b6ecade9f4ecc0d5ff70c4d6ee244
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2023-12-24 13:35:27 +0530

    Time: 6 ms (35.61%) | Memory: 7.6 MB (12.76%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int ans1=0, ans2=0;
+        int next ;
+        // the mistake of yours is actually we have to find minimum no of flips to make alternate bibary string
+        //count
+        next = 0;
+        for(int i=0; i< s.size(); i++)
+        {
+            if(s[i] - '0' != next)
+            {
+                ans1++;
+            }
+
+            next = (next + 1) % 2;
+        }
+
+        // now checking for ans in case we start alterenating string from 1
+        next=1;
+        for(int i=0; i< s.size(); i++)
+        {
+            if(s[i] - '0' != next)
+            {
+                ans2++;
+            }
+
+            next = (next + 1) % 2;
+        }
+
+        return min(ans1,ans2);
+    }
+};
