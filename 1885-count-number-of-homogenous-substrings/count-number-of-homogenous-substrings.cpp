4ea3a4b7b8c20780b93c69923d43581238678b35
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2023-12-28 15:24:59 +0530

    Time: 42 ms (6.79%) | Memory: 15.1 MB (6.06%) - LeetSync

---
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countHomogenous(string s) {
+       //applying two pointers to extarct out sizes of substring sizes with same letters 
+
+       //size vector
+        vector<int> subSize;
+
+        int i=0,j=0,n=s.size();
+
+        while(j < n)
+        {
+            if(s[j] != s[i])
+            {
+                subSize.push_back(j-i);
+                i=j;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+        // inserting size of last substring
+
+        subSize.push_back(j - i);
+
+        long res=0,m;
+
+        for(int i=0; i< subSize.size(); i++)
+        {
+            m = subSize[i];
+            res += m * (m+1)/2; 
+        }
+        return res % (int)(pow(10,9)+7 );
+    }
+};
