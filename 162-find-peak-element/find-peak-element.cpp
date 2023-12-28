267a85ba5a1daa2a836e788d77e18e8eef2690f1
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2023-12-28 13:51:19 +0530

    Time: 13 ms (32.92%) | Memory: 9.2 MB (73.63%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        //element at last of each sorted subarray is the ans
-        int j=0,res,n = nums.size();
-        while( j< n-1 )
+        int n = nums.size();
+
+        if(n == 1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+        //Applying Binary search without sorting :) ->
+        int s=1, e= n-2;
+        int mid = s+(e-s)/2;
+        while(s <= e)
         {
-            if(nums[j+1] < nums[j])
+            mid = s+(e-s)/2;
+
+            if(nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1])
             {
-                res = j;
                 break;
             }
-            else if(nums[j+1] >= nums[j])
+            else if(nums[mid] < nums[mid+1])
             {
-                j++;
+                s = mid+1;
+            }
+            else if(nums[mid] < nums[mid-1])
+            {
+                e= mid-1;
             }
         }
-        return res;
+        return mid;
     }
 };
