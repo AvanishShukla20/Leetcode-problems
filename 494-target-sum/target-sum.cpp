73cd1a9702057428e97348b0161ea5e9534eaf3b
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2023-12-30 14:20:48 +0530

    Time: 1010 ms (23.18%) | Memory: 9.4 MB (80.18%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int CountWays(vector<int>& nums,int idx, int target,int count)
+    {
+        if(idx == nums.size())
+        {
+            if(count == target) return 1;
+            else return 0;
+        }
+
+        int x =nums[idx];
+        int sumWay  = CountWays(nums, idx+1, target, count + x);
+        int diffWay = CountWays(nums, idx+1, target, count - x);
+
+        int totalWay = sumWay + diffWay ;
+        return totalWay;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return CountWays(nums,0,target,0);
+        
+    }
+};
