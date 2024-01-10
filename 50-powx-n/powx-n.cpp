3edb75b0fe87adbe563728f778e75bfb4b0fc9b3
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2024-01-10 22:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (60.64%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double calculateRes(double x, int n)
+    {
+        if(n==0)
+        {
+            return 1.0;
+        }
+        double res = myPow(x, n/2);
+
+        res *= res;
+        if(n%2 !=0) res *= x;
+
+        return res;
+    }
+    double myPow(double x, int n) {
+        double ans = calculateRes(x,abs(n));
+        if(n<0) return 1.00/ans;
+        else return ans;
+    }
+};
