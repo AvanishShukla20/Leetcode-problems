e315b353d463c0a239743413a8aa2a7a8cb99ac0
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2024-01-18 00:16:51 +0530

    Time: 41 ms (5.29%) | Memory: 180.9 MB (5.38%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
+  void res(int i1, int i2, string num1, string num2, int carry, string& ans)
+    {
+        if(i1 < 0 && i2 < 0 && carry == 0)
+        {
+           return;
+        }
+        int sum=0;
+        if(i1 >= 0) sum += num1[i1]-'0';
+        if(i2 >= 0) sum += num2[i2]-'0';
+        sum += carry;
+        if(sum > 9) carry = 1;
+        else carry = 0;
+        sum %= 10;
+        ans += sum+'0';
+        res(i1-1, i2-1, num1, num2, carry, ans); 
+    }
 
-    
     string addStrings(string num1, string num2) {
-
        string ans = "";
-       int carry = 0, n = num1.size(), m = num2.size();
-       int i = n-1, j = m-1;
-       while(i >= 0 || j >= 0)
-       {
-           int sum = 0;
-           if(i >= 0)
-           {
-               sum += num1[i]-'0';
-               i--;
-           }
+       
+       /* Obvious Mistake of a beginner Step ->  res(0,0, num1, num2, 0, ans); */
+        //master plan -> we are starting our function calls starting from last indices first
 
-           if(j >= 0)
-           {
-              
-               sum += num2[j]-'0';
-               j--;
-           }
-
-           sum += carry;
-           if(sum > 9) carry = 1;
-           else carry = 0;
-           sum %= 10;
-           ans += sum+'0';
-       }
-       if(carry != 0) ans += '1';
-       reverse(ans.begin(), ans.end()); 
-       return ans;
+        res(num1.size()-1, num2.size()-1, num1, num2, 0,ans);
+        reverse(ans.begin(), ans.end());
+        return ans;
 
     }
 };
