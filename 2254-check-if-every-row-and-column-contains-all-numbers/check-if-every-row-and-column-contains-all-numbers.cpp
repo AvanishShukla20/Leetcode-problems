0da4984fc835af56e437bf9de0afe2514eb57c2a
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2024-01-22 02:39:28 +0530

    Time: 241 ms (36.18%) | Memory: 96.7 MB (5.98%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
-
-        sort(matrix.begin(), matrix.end());
-        set<int> s1;
-        for(int i=1; i<= matrix.size(); i++) s1.insert(i);
-
-         // column check
-        for(int i=0; i< matrix.size(); i++)
+        //we will use two sets and We will not initialise any of them in beginning 
+        set<int> sRow,sCol;
+     
+        for(int i=0; i<matrix.size(); i++)
         {
-            set<int>s(s1.begin(),s1.end());
-
-            for(int j=0; j<matrix.size(); j++)
+            for(int j = 0; j<matrix.size(); j++)
             {
-                if(s.find(matrix[j][i]) == s.end())return false;
-                s.erase(matrix[j][i]);  
+                if(sRow.find(matrix[i][j]) != sRow.end()) return false;
+                else sRow.insert(matrix[i][j]);
+                //no need to reinitialise the set multiple times as you were doing in your previous worst case code
+
+                if(sCol.find(matrix[j][i]) != sCol.end()) return false;
+                else sCol.insert(matrix[j][i]);
             }
-
+            //THIS STEP YOU ALWAYS FORGET-> Do as it saves Lots of memory ->
+            sRow.clear();
+            sCol.clear();
         }
-
-        //row check
-        for(int i=0; i< matrix.size(); i++)
-        {
-            set<int>s(s1.begin(),s1.end());
-            for(int j=0; j<matrix.size(); j++)
-            {
-                if(s.find(matrix[i][j]) == s.end()) return false;
-                s.erase(matrix[i][j]); 
-
-            }
-        }
-
         return true;
     }
 };
