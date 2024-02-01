5897db983e2ead1701a79b6ce74e02c6ca5d7ad8
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2024-02-01 17:16:30 +0530

    Time: 208 ms (31.28%) | Memory: 119.4 MB (39.48%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -4,11 +4,12 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
         int  n = nums.size();
-        for(int i = 0 ; i < n; i = i+3)
+        for(int i = 0 ; i < n - 2; i = i+3)
         {
-            if(i < n-2 and nums[i + 2] - nums[i] <= k)
+            if(nums[i + 2] - nums[i] <= k)
             {
                 //we can form 3 elements upto i+2 index
+                cout<<"ran this loop : "<<endl;
                 ans.push_back({nums[i], nums[i+1], nums[i+2]});
             }
             else return {};
