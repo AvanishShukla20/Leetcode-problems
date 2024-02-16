17fb49c88937c430549d773f5fbbd4dea8eff288
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2024-02-16 17:30:04 +0530

    Time: 143 ms (67.16%) | Memory: 125.6 MB (85.36%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-      vector<int> ans;
+        int n =nums.size();
+        int greaterPos = 0;
 
-        for(int i=0 ;i<nums.size(); i++)
+        // finding pos for first greater element
+        for(int i=0; i<n; i++)
         {
-        if(nums[i] < pivot) ans.push_back(nums[i]);
+            if(nums[i] <= pivot) greaterPos++;
         }
 
-        for(int i=0 ;i<nums.size(); i++)
+        int lessPos = 0;   
+        //equal will have its position just before the greater one
+        int equalPos = greaterPos - 1;
+
+
+        vector<int> ans(n);
+
+        for(int j =0; j< n; j++)
         {
-        if(nums[i] == pivot) ans.push_back(nums[i]);
+            if(nums[j] < pivot)
+            {
+                ans[lessPos] = nums[j];
+                lessPos++;
+            }
+            else if(nums[j] > pivot)
+            {
+                ans[greaterPos] = nums[j];
+                greaterPos++;
+            }
+            else
+            {
+                //for equal to pivot
+                ans[equalPos] = nums[j];
+                equalPos--;
+            }
         }
 
-        for(int i=0 ;i<nums.size(); i++)
-        {
-        if(nums[i] > pivot) ans.push_back(nums[i]);
-        }
+        
         return ans; 
     }
 };
