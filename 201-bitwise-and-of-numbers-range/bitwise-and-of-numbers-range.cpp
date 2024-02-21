533e8dd9fe568cde877f7341580770fca7e45093
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2024-02-21 22:48:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (55.90%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -3,6 +3,7 @@ public:
     int rangeBitwiseAnd(int left, int right) {   
         while(right > left)
         {
+            //removing leftmost set bit each time and initialise that value into right again
             right = right & (right - 1);
         }
 
