b643a1a064d5caabbe23cb75f41a9d20e207516f
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2024-02-25 15:39:28 +0530

    Time: 26 ms (49.49%) | Memory: 29.2 MB (50.79%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int lHeight(TreeNode* curr)
+    {
+        //iterative ->
+        int ans = 0;
+        while(curr)
+        {
+            ans++;
+            curr = curr->left;
+        }
+        return ans;
+    }
+
+    int rHeight(TreeNode* curr)
+    {
+        //iterative ->
+        int ans = 0;
+        while(curr)
+        {
+            ans++;
+            curr = curr->right;
+        }
+        return ans;
+    }
+    int countNodes(TreeNode* root) {
+        if(root == NULL) return 0;
+
+
+        int leftHeight = lHeight(root);
+        int rightHeight = rHeight(root);
+
+        if(leftHeight == rightHeight)
+        {
+            //no further countNodes function call 
+            return pow(2, leftHeight) - 1;
+        }
+        else
+        {
+            return 1 + countNodes(root->left) + countNodes(root->right);
+        }
+
+    }
+};
