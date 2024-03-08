dfe025c6fd22f450d4202fbc639d7dbfae345d52
AvanishShukla20 <144734552+AvanishShukla20@users.noreply.github.com>
2024-03-08 17:35:39 +0530

    Time: 27 ms (48.33%) | Memory: 14 MB (11.61%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -3,13 +3,17 @@ public:
     int minimumLength(string s) {
        int n = s.size();
        int i=0, j=n-1;
-       while(i < j)
+       while(i < j) // not put i <= j as i can't make prefix and suffix intersect
        {
            if(s[i] == s[j])
            {
-                char x = s[i];
-                while(i<=j &&s[i] == x) i++;
-                while(j>=i && s[j] == x) j--;
+                char x = s[i]; 
+                /* In every loop you have to again pust i<j type constraint because
+                YOU CAN never let your pointers move endlessly just by checking 
+                that s[i] or s[j] == x .HENCE AT EVERY STEP WE MUST APPLY CONSTRAINTS
+                IN BOTH THE WHILE LOOPS BELOW */
+                while(i < j &&s[i] == x) i++;
+                while( i <= j && s[j] == x) j--; // MAIN CATCH -> note the "<=" condition is there not "<"
            }
           else break;   
        }
