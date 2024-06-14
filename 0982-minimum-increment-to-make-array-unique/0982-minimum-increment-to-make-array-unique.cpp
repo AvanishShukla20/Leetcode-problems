c94b2c3150874f241fce0e600617d59af0b5b79e
Avanish Shukla <avanishshukla2021@gmail.com>
2024-06-14 11:44:16 +0530

    Time: 85 ms (96.85%), Space: 74 MB (12.4%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int ans = 0;
-        for(int i=1; i<nums.size(); i++)
-        {
-            if(nums[i-1] == nums[i])
-            {
-                ans += 1;
-                nums[i] = nums[i]+1;
-            }
-            else if(nums[i-1] > nums[i]) // previous = 3 current = 2
-            {
-                ans += nums[i-1] + 1 - nums[i];
+        // make hash array of size = size of nums + max element of nums
+        int n = nums.size();
+        int m = *max_element(nums.begin(), nums.end());
+        vector<int> freq(n + m+1, 0);
 
-                nums[i] = nums[i-1] + 1;
-            }
+
+        for(int i =0; i < n; i++)
+        {
+            freq[nums[i]]++;
         }
 
-        return ans;
+        int ans =0, extra;
 
+        for(int i=0; i<n+m+1; i++)
+        {
+            // i.e either doesn't exists or unique exists
+            if(freq[i] <= 1) continue; 
+            else
+            {
+                //duplicate exists
+                //shift the extra frequency to next index element in freq
+                extra = freq[i] - 1;
+                freq[i+1] += extra;
+                ans += extra;
+            }
+        }
+        return ans;
 
     }
 };
