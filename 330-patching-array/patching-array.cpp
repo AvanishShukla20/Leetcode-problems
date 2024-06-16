0654fe67a0054fb6775925bb048839549ecee75f
Avanish Shukla <avanishshukla2021@gmail.com>
2024-06-17 01:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (45.23%) - LeetSync

---
+++ 330-patching-array/patching-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long patchCount = 0, i = 0, m = nums.size();
+        long  maxSumAchieved = 1;
+
+        while(maxSumAchieved <= n)
+        {
+            if(i < m && nums[i] <= maxSumAchieved)
+            {
+                maxSumAchieved += nums[i];
+                i++;
+            }
+            else 
+            {
+                maxSumAchieved += maxSumAchieved;
+                patchCount++;    
+            }
+        }
+        return patchCount ;
+    }
+};
