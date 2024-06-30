a93af4af542d34f5e82a6f2c40cfb8c93dc958b8
Avanish Shukla <avanishshukla2021@gmail.com>
2024-06-30 23:44:36 +0530

    Time: 8 ms (9.58%) | Memory: 13.5 MB (37.45%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    int BinSearch(vector<int>& nums, int low, int high, int target)
-    {
-        int mid;
+    int search(vector<int>& nums, int target) {
+        // main idea is to search in sorted portion of array and eliminate and update search space accordingly
+        int n = nums.size();
+        int low = 0, high = n - 1;
+
         while(low <= high)
         {
-            mid = low+(high-low)/2;
+            int mid = low + (high -low)/2;
+
             if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) low = mid+1;
-            else high = mid-1;
-        } 
-        return -1;
-    }
 
-    // main learning of this Ques
-    int findPivot(vector<int>& arr, int left, int right)
-    {
-        int mid;
-        while(left < right)
-        {
-            mid = left + (right-left)/2;
-
-            if(arr[mid] > arr[right]) left = mid+1;
-            else right = mid;
+            if(nums[low] <= nums[mid]) // if left portion is sorted
+            {
+                if(nums[low] <= target && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            else if(nums[mid] <= nums[high])
+            {
+                if(nums[mid] <= target && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
         }
-        return left;
-    }
-    int search(vector<int>& nums, int target) {
-        // right -> last idx
-        int n = nums.size();
-        int pivotIdx = findPivot(nums, 0, n-1);
-        
-        int ans = -1;
-        // apply binsearch from 0 to pivot-1
-        ans = BinSearch(nums, 0, pivotIdx-1,target);
-        
-        if(ans == -1) ans = BinSearch(nums, pivotIdx, n-1,target);
-        
-        return ans;
 
+        return -1;
     }
 };
