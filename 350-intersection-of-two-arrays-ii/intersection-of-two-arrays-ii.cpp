71a201333a18bfcd03f4ef04732f40ee98528243
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-02 16:34:52 +0530

    Time: 7 ms (27.72%) | Memory: 12.5 MB (75.54%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,49 +1,25 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
 
-        int m =nums1.size(), n = nums2.size(), target;
-        if(m < n) target = m;
-        else target = n;
-
-        int i = 0, j = 0;
         vector<int> ans;
-        if(target == n)
+
+        int i=0, j = 0;
+
+        while(i < m && j < n)
         {
-            while(i < m && j < n)
+            if(nums1[i] == nums2[j])
             {
-                
-                if(nums1[i] >= nums2[j])
-                {
-                    if(nums1[i] == nums2[j])
-                    {
-                        ans.push_back(nums1[i]);
-                        i++;
-                    }
-                    j++;
-                }
-                else i++;
+                ans.push_back(nums1[i]);
+                i++,j++;
             }
+            else if(nums1[i] < nums2[j]) i++;
+            else j++;
         }
-        else
-        {
-            while(i < n && j < m)
-            {
-                
-                if(nums2[i] >= nums1[j])
-                {
-                    if(nums2[i] == nums1[j])
-                    {
-                        ans.push_back(nums2[i]);
-                        i++;
-                    }
-                    j++;
-                }
-                else i++;
-            }
-        }
+
         return ans;
     }
 };
