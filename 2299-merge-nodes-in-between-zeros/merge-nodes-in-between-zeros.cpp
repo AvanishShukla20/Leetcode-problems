29dc6cdb63233d789ceff77a9e19d539bc67f9c6
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-05 12:27:17 +0530

    Time: 458 ms (40.10%) | Memory: 278.1 MB (19.00%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -11,25 +11,34 @@
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-        ListNode* modify = head->next; // Start from the node after the initial 0
-        ListNode* nextSum = modify;
-
-        while (nextSum != nullptr) {
-            int sum = 0;
-            // Find the sum of all nodes until you encounter a 0.
-            while (nextSum->val != 0) {
-                sum += nextSum->val;
-                nextSum = nextSum->next;
+        if(!head) return NULL;
+        int sum = 0;
+        bool headmade = 0;
+        head = head->next;
+        ListNode* nhead = new ListNode();
+        ListNode* temp = NULL;
+        while(head)
+        {
+            sum += head->val;
+            if(head->val == 0)
+            {
+                if(!headmade)
+                {
+                    nhead->val = sum;
+                    temp = nhead;
+                    headmade = 1;
+                    sum = 0;
+                }
+                else
+                {
+                    temp->next = new ListNode(sum);
+                    temp = temp->next;
+                    sum = 0;
+                }
             }
-
-            // Assign the sum to the current node's value.
-            modify->val = sum;
-            // Move nextSum to the first non-zero value of the next block.
-            nextSum = nextSum->next;
-            // Move modify also to this node.
-            modify->next = nextSum;
-            modify = modify->next;
+            head = head->next;
         }
-        return head->next; // Skip the initial 0 node.
+
+        return nhead;
     }
 };
