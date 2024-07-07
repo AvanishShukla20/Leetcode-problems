0af6f9542089bbcc0b85d741d9505414ec8b5c38
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-07 13:26:19 +0530

    Time: 3 ms (13.78%) | Memory: 7.2 MB (30.43%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int ans = numBottles, full = numBottles, done;
-
-        while(full >= numExchange)
-        {
-            done = full/numExchange;
-            ans += done;
-            int rem = full%numExchange;
-
-            full = done + rem;
-        }
+        int ans = numBottles + (numBottles-1) / (numExchange-1);
+        
         return ans;
     }
 };
