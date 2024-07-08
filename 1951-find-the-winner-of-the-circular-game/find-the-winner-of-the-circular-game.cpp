ebdf3ab3b667e87720f2e969529b79035df696d6
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-08 17:09:31 +0530

    Time: 24 ms (22.70%) | Memory: 25.7 MB (9.19%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        //chhose some elements in circular list and dlete some then perform same operation on remaining elements in the list unti you got only one person . return info of this person
+        // Fantastic : Use of Queue to find idxToRemove;
+        queue<int> Q;
 
-        vector<int> friends(n);
-        for(int i =0; i<n ;i++)
+        for(int i = 1; i <= n; i++) Q.push(i);
+
+        while(Q.size() > 1)
         {
-            friends[i] = i + 1;
+            for(int i = 0; i < k - 1; i++)
+            {
+                Q.push(Q.front());
+                Q.pop();
+            }
+
+            // remove the top element 
+            Q.pop();
         }
 
-        int start = 0, friendsRemaining = friends.size(), idxtoremove;
-
-        while(friendsRemaining > 1)
-        {
-            idxtoremove = (start + k - 1) % friendsRemaining;
-
-            friends.erase(friends.begin() + idxtoremove);
-
-            friendsRemaining = friends.size();
-
-            start = idxtoremove;
-        }
-
-        return friends[0];
+        return Q.front();
     }
 };
