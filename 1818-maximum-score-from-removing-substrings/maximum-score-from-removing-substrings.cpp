a74a5ff45a37789e63d9f21421dc6786fa02b2d3
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-12 20:33:43 +0530

    Time: 118 ms (22.36%) | Memory: 30.7 MB (9.76%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string solve(string target,string& s,int score, int& ans)
+    {
+        stack<char> st;
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == target[1] && !st.empty() && st.top() == target[0])
+            {
+                ans += score;
+                st.pop(); 
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+        }
+
+        string str = "";
+
+        while(!st.empty())
+        {
+            str += st.top();
+            st.pop();
+        }
+        reverse(str.begin(), str.end());
+        return str;
+    }
+    int maximumGain(string s, int x, int y) {
+        
+        int ans = 0;
+
+        int maxlen = (x > y) ? x : y;
+
+        if(maxlen == y)
+        {
+            string received = solve("ba",s, y, ans);
+
+            string temp = solve("ab",received, x, ans);
+            //temp is of no use further
+        }
+        else
+        {
+            string received = solve("ab",s, x, ans);
+            string temp = solve("ba",received, y, ans);
+            //temp is of no use further
+        }
+        
+        return ans;
+        
+    }
+};
