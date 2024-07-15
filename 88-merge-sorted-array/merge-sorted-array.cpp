03727ffc7275d7e59cdda4d7e79b0a043d345d44
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-15 12:35:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (9.90%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int zeropos = -1;
+        
+        int j = n - 1;
+        int i = m - 1;
+        int k = m + n - 1;
 
-        for(int i = nums1.size() - 1; i >= 0; i--)
+        while(j >= 0)
         {
-            if(nums1[i] != 0)
+            if(i >= 0 && nums1[i] >= nums2[j])
             {
-                zeropos = i;
-                break;
-            }
-        }
-
-
-        for(int j = 0; j<nums2.size(); j++)
-        {
-            nums1[zeropos + 1] = nums2[j];
-
-            //placing at correct pos
-            int i = zeropos + 1;
-            while(i > 0)
-            {
-                if(nums1[i-1] > nums1[i])
-                {
-                    swap(nums1[i-1], nums1[i]);
-                }
-                else break;
+                nums1[k] = nums1[i];
                 i--;
             }
+            else
+            {
+                nums1[k] = nums2[j];
+                j--;
+            }
 
-            zeropos += 1;
+            k--;
         }
-
     }
 };
