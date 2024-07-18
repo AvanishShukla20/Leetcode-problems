5331394bc35b07fcbf9d8e93a305f473d775588d
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-19 00:13:42 +0530

    Time: 253 ms (18.46%) | Memory: 159.5 MB (11.29%) - LeetSync

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -1,94 +1,98 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+
+    TreeNode* lca(TreeNode* root, TreeNode*& a, TreeNode*& b)
+    {
+        if(!root) return NULL;
+        else if(root == a || root == b) return root;
+
+        TreeNode* left = lca(root->left, a, b);
+        TreeNode* right = lca(root->right, a, b);
+
+        if(left && right) return root;
+        else if(left != NULL) return left;
+        else return right;
+    }
+
+    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& parent, int& startValue,int& destValue, TreeNode*& start, TreeNode*& dest)
+    {
+        if(!root) return;
+
+        if(root->val == startValue) start = root;
+        if(root->val == destValue) dest = root;
+
+        if(root->left)
+        {
+            parent[root->left] = root;
+            markParents(root->left, parent,startValue, destValue, start, dest);
+        }
+        if(root->right)
+        {
+            parent[root->right] = root;
+            markParents(root->right, parent,startValue, destValue, start, dest);
+        }
+    }
+
+
     string getDirections(TreeNode* root, int startValue, int destValue) {
+
+        unordered_map<TreeNode*, TreeNode*> parent;
+        TreeNode* start = NULL;
+        TreeNode* dest = NULL;
+        markParents(root, parent, startValue, destValue, start, dest);
+
+        
+        TreeNode* lcanode = lca(root, start, dest);
+        cout<<start->val<<" "<<dest->val<<" "<<lcanode->val<<endl;
+        // going up from start to lca
+
+        string ans1 = "";
         queue<TreeNode*> q;
-        q.push(root);
-        TreeNode* start_node = nullptr;
+        q.push(start);
 
-        while (!q.empty()) {
-            TreeNode* cur_node = q.front();
+        while(!q.empty())
+        {
+            auto it = q.front();
             q.pop();
+            if(it == lcanode) break;
+            ans1 += 'U';
+            q.push(parent[it]);
+        }
+        cout<<" ans1 :"<<ans1<<endl;
 
-            if (cur_node->val == startValue) {
-                start_node = cur_node;
-                break;
+        //going up from dest to lca
+        queue<TreeNode*> q1;
+        string ans2 = "";
+        q1.push(dest);
+        while(!q1.empty())
+        {
+            auto it = q1.front();
+            q1.pop();
+            if(it == lcanode) break;
+            if(parent[it]->left == it)
+            {
+                ans2 += 'L';
             }
-
-            if (cur_node->left) {
-                q.push(cur_node->left);
-            }
-            if (cur_node->right) {
-                q.push(cur_node->right);
-            }
+            else ans2 += 'R';
+            q1.push(parent[it]); 
         }
 
-        unordered_map<int, TreeNode*> nodes_parents;
-        q.push(root);
+        reverse(ans2.begin(), ans2.end());
 
-        while (!q.empty()) {
-            TreeNode* cur_node = q.front();
-            q.pop();
+        string ans = ans1 + ans2;
+        return ans;
 
-            if (cur_node->left) {
-                nodes_parents[cur_node->left->val] = cur_node;
-                q.push(cur_node->left);
-            }
-            if (cur_node->right) {
-                nodes_parents[cur_node->right->val] = cur_node;
-                q.push(cur_node->right);
-            }
-        }
 
-        unordered_set<TreeNode*> visited;
-        q.push(start_node);
-        unordered_map<TreeNode*, pair<TreeNode*, char>> tracked_path;
-
-        TreeNode* destination_node = nullptr;
-
-        while (!q.empty()) {
-            TreeNode* cur_node = q.front();
-            q.pop();
-            visited.insert(cur_node);
-
-            if (cur_node->val == destValue) {
-                destination_node = cur_node;
-                break;
-            }
-
-            if (nodes_parents.find(cur_node->val) != nodes_parents.end() && visited.find(nodes_parents[cur_node->val]) == visited.end()) {
-                TreeNode* parent = nodes_parents[cur_node->val];
-                q.push(parent);
-                tracked_path[parent] = make_pair(cur_node, 'U'); 
-            }
-
-            if (cur_node->left && visited.find(cur_node->left) == visited.end()) {
-                q.push(cur_node->left);
-                tracked_path[cur_node->left] = make_pair(cur_node, 'L'); 
-            }
-
-            if (cur_node->right && visited.find(cur_node->right) == visited.end()) {
-                q.push(cur_node->right);
-                tracked_path[cur_node->right] = make_pair(cur_node, 'R'); 
-            }
-        }
-
-        stack<char> result_path;
-        TreeNode* cur_node = destination_node;
-
-        while (cur_node != start_node) {
-            auto it = tracked_path.find(cur_node);
-            if (it != tracked_path.end()) {
-                result_path.push(it->second.second); 
-                cur_node = it->second.first;
-            }
-        }
-
-        string res;
-        while (!result_path.empty()) {
-            res += result_path.top();
-            result_path.pop();
-        }
-
-        return res;
     }
 };
