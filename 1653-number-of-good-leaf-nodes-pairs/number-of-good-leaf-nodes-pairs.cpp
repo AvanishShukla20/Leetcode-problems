26b2242c3797888cc03ad661fc6574ded3eca579
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-18 22:57:52 +0530

    Time: 55 ms (36.51%) | Memory: 36.2 MB (29.84%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
@@ -11,18 +11,7 @@
  */
 class Solution {
 public:
-    // TreeNode* lca(TreeNode* root, TreeNode*& a, TreeNode*& b)
-    // {
-    //     if(!root) return NULL;
-    //     else if(root == a || root == b) return root;
-
-    //     TreeNode* left = lca(root->left, a, b);
-    //     TreeNode* right = lca(root->right, a, b);
-
-    //     if(left && right) return root;
-    //     else if(left != NULL) return left;
-    //     else return right;
-    // }
+    
 
     vector<int> helper(TreeNode* root,int& distance, int& cnt)
     {
@@ -47,32 +36,9 @@ public:
     }
 
 
-    // void parent(TreeNode* root,unordered_map<TreeNode*, TreeNode*>& mpp, vector<TreeNode*>& leaves)
-    // {
-    //     queue<TreeNode*> q;
-    //     q.push(root);
-    //     while(!q.empty())
-    //     {
-    //         auto node = q.front();
-    //         if(!node->left && !node->right) leaves.push_back(node);
-
-    //         q.pop();
-    //         if(node->left)
-    //         {
-    //             mpp[node->left] = node;
-    //             q.push(node->left);
-    //         }
-    //         if(node->right)
-    //         {
-    //             mpp[node->right] = node;
-    //             q.push(node->right);
-    //         }
-    //     }
-    // }
-
+    
 
     int countPairs(TreeNode* root, int distance) {
-        unordered_map<TreeNode*,TreeNode*> mpp;
         int cnt = 0;
         vector<int> r = helper(root,distance, cnt);
 
