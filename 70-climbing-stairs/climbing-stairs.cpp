f4a2b2b10eb16c1b6301f95da579bbd97f7e1232
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-19 17:14:58 +0530

    Time: 2 ms (49.56%) | Memory: 7.4 MB (39.36%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    // Here in this Ques You learn a memoization technique to drastically reduce the number of function callls with same parameters
+    
+    int helper(int n, int dp[])
+    {
+        if(n < 0) return 0;
+        if(n == 0) return 1;
+        if(dp[n] != -1) return dp[n];
 
-    //we take help of a storage data structure to update values of obtained function call values in it
-
-
-    int calculateSteps(int n, unordered_map< int,int >& map){
-        // base case 
-        if(n==0 || n==1)
-        {
-            map[n] = 1;
-            return map[n] ;
-        } 
-        else if( map.find(n) == map.end())
-        {
-            map[n] = calculateSteps(n-1, map) + calculateSteps(n-2, map);
-        }
-        return map[n];
+        return dp[n] = helper(n-1, dp) + helper(n-2, dp);
     }
-
     int climbStairs(int n) {
-        unordered_map< int,int > mpp;
-        int res = calculateSteps(n, mpp);
-        return res;
+        int dp[46];
+        memset(dp, -1, sizeof dp);
+        return helper(n, dp);
     }
 };
