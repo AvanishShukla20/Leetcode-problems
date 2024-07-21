794ab283f9e50f3c5c85facc45c62c1da111cb4c
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-21 17:45:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (10.49%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int solve(int idx, vector<int>& nums, vector<int>& dp)
     {
-        if(idx >= nums.size()) return 0;
+        if(idx < 0) return 0;
 
         if(dp[idx] != -1) return dp[idx];
         //include
-        int left = solve(idx + 2, nums, dp) + nums[idx];
+        int left = solve(idx - 2, nums, dp) + nums[idx];
         //exclude
-        int right = solve(idx + 1, nums, dp);
+        int right = solve(idx - 1, nums, dp);
 
         return dp[idx] = max(left, right);
     }
     int rob(vector<int>& nums) {
 
         vector<int> dp(nums.size(), -1);
-        return solve(0, nums, dp);
+        return solve(nums.size() - 1, nums, dp);
     }
 };
