47082adde9a08e717ad3ea78caa131fb94d89611
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-21 18:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (5.17%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,15 +6,14 @@ public:
         vector<int> dp(n, -1);
 
         dp[0] = nums[0];
-        int neg = 0;
+        
         for(int i = 1; i < n; i++)
         {
 
             int inc = dp[i - 1];
-            
-            int exc = 0;
-            if(i > 1) exc = nums[i] + dp[i - 2];
-            else exc = nums[i] + neg; // noob ki tarh likha taaki smjho
+
+            int exc = nums[i];
+            if(i > 1) exc += dp[i - 2];
 
             dp[i] = max(inc, exc);
         }
