9dbbebcd62bbd5ecc5630f20cdbaa1b90ece779d
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-23 20:21:47 +0530

    Time: 3 ms (13.48%) | Memory: 7.4 MB (15.87%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
+    
     int kthGrammar(int n, int k) {
-       if(n == 1 && k == 1) return 0;
+        if(n == 1) return 0;
+        int middlepos = pow(2, n -2);
 
-       //calculated mid by observation
-       int mid = pow(2,n-1)/2 ;
-
-       if(k <= mid)
-       {
-           //upto mid term the curr string is copy of previous string So same k will give answer
-           return kthGrammar(n-1,k);
-       }
-       //The other half is the copy of Complement of previous string
-       else
-       {
-           return kthGrammar(n-1, k - mid)^1; // flipping the bit returned by kthgrammar function using XOR
-       } 
+        if(k <= middlepos)
+        {
+            return kthGrammar(n - 1, k);
+        }
+        else
+        {
+            int r = kthGrammar(n - 1, k - middlepos);
+            return r^1;
+        }
     }
 };
