87b19684de83d5142619de444b2e565502764d13
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-23 22:24:13 +0530

    Time: 3 ms (53.92%) | Memory: 10.5 MB (10.24%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -4,25 +4,30 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
-        if(obstacleGrid[m-1][n-1] == 1) return 0;
-        
-        vector<vector<int>> dp(m, vector<int> (n, 0));
-
-        dp[0][0] = 1;
-        
-        for(int i =0; i<m; i++)
+        vector<int> dp(n, 0);
+        for(int i=0; i<n; i++)
         {
-            for(int j =0; j< n; j++)
-            {
-                if(i == 0 && j == 0) continue;
-                int up = 0, left = 0;
-
-                if(i > 0 && obstacleGrid[i-1][j] == 0) up = dp[i - 1][j];
-                if(j > 0 && obstacleGrid[i][j - 1] == 0) left = dp[i][j - 1];
-                dp[i][j] = up + left;
-            } 
+            if(obstacleGrid[0][i] != 0) break;
+            else dp[i]++;
         }
 
-        return dp[m-1][n-1];
+        for(int row = 1; row < m; row++)
+        {
+            vector<int> current(n, 0);
+
+            for(int col = 0; col < n; col++)
+            {
+                int up = 0, left = 0;
+                if(obstacleGrid[row][col] == 0)
+                {
+                    if(col > 0) left += current[col-1];
+                    up += dp[col];
+                }
+                current[col] = up + left;
+            }
+            dp = current;
+        }
+
+        return dp[n - 1];
     }
 };
