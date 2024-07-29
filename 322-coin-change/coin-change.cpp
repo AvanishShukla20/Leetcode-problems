47d23f242e1d080f3d44cf5519628b6c4f902bd9
Avanish Shukla <avanishshukla2021@gmail.com>
2024-07-29 15:34:39 +0530

    Time: 42 ms (44.60%) | Memory: 47.1 MB (34.17%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int minCoins(int idx, int target, vector<int>& coins, vector<vector<int>>& dp)
-    {
-        if(idx == 0)
-        {
-            if(target%coins[idx] == 0) return target/coins[idx];
-            else return 1e9;
-        }
-
-        if(dp[idx][target] != -1) return dp[idx][target];
-
-        int non_take = minCoins(idx - 1, target, coins, dp) + 0;
-        int take = 1e9;
-        if(target >= coins[idx]) take = 1 + minCoins(idx, target - coins[idx], coins, dp);
-
-        return dp[idx][target] = min(take, non_take);
-    }
+    
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-        int res = minCoins(n-1, amount, coins, dp);
-        if(res == 1e9) return -1;
-        return res;
+        vector<vector<int>> dp(n, vector<int> (amount+1, 1e9));
+
+        for(int i = 0; i<= amount; i++)
+        {
+            if(i%coins[0] == 0) dp[0][i] = i/coins[0];
+        }
+
+
+        for(int idx = 1; idx < n; idx++)
+        {
+            for(int target = 0; target <= amount; target++)
+            {
+                int non_take = dp[idx - 1][target];
+                int take = 1e9;
+                if(target >= coins[idx]) take = 1 + dp[idx][target- coins[idx]];
+
+                dp[idx][target] = min(take, non_take);
+            }
+        }
+
+
+        return (dp[n-1][amount] == 1e9) ? -1 : dp[n-1][amount];
     }
 };
