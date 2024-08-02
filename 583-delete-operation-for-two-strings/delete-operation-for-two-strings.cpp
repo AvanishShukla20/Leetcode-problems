8e52fc0564562e8288ecd011e2c2c07685d3310c
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-02 14:21:21 +0530

    Time: 873 ms (5.04%) | Memory: 739.1 MB (5.01%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int lcs(int i, int j, string s1, string s2, vector<vector<int>>& dp)
     {
-        if(i < 0 || j < 0) return 0;
+        if(i == 0 || j == 0) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
         //match
 
-        if(s1[i] == s2[j]) return 1 + lcs(i-1,j-1, s1, s2, dp);
+        if(s1[i - 1] == s2[j - 1]) return 1 + lcs(i-1,j-1, s1, s2, dp);
 
         int not_match = 0 + max(lcs(i-1, j, s1, s2, dp), lcs(i, j-1, s1, s2, dp));
 
@@ -18,7 +18,7 @@ public:
         int n = word1.size(), m = word2.size();
         vector<vector<int>> dp(n + 1, vector<int> (m+1, -1));
 
-        int Longest_common_len = lcs(n - 1, m - 1, word1, word2, dp);
+        int Longest_common_len = lcs(n, m, word1, word2, dp);
         cout<<Longest_common_len<<endl;
 
         int ans = n + m - 2*Longest_common_len;
