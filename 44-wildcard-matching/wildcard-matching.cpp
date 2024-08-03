72f5fada51f8f7ef827b7565ac7430f21658f9e6
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-03 18:12:34 +0530

    Time: 69 ms (25.54%) | Memory: 14.7 MB (77.68%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    bool solve(int i, int j, string& s1, string& s2, vector<vector<int>>& dp)
-    {
-        if(i == 0)
-        {
-            if(j == 0) return true;
-            return false;
-        }
-
-        if(j == 0)
-        {
-            //the all remaining i's MUST be '*' for returning true;
-            for(int p = 1; p <= i; p++)
-            {
-                if(s1[p - 1] != '*') return false;
-            }
-            return true;
-        }
-
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        //recurrence ->
-
-        if(s1[i-1] == s2[j-1] || s1[i-1] == '?') return dp[i][j] = solve(i-1, j-1, s1, s2, dp);
-        if(s1[i-1] == '*')
-        {
-            return dp[i][j] = solve(i, j-1, s1, s2, dp) | solve(i-1, j, s1, s2, dp); 
-        }
-        return dp[i][j] = false;
-    }
     bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
-        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
-        return solve(m, n, p, s, dp);
+        vector<vector<bool>> dp(m+1, vector<bool> (n+1, false));
+
+        dp[0][0] = true;
+
+        for(int i = 1; i<= m; i++)
+        {
+            bool flag = true;
+            //the all remaining i's MUST be '*' for returning true
+            for(int w = 1; w <= i; w++) // because now both p and i are 1 based indexing
+            {
+                if(p[w - 1] != '*')
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;
+        }
+
+        for(int i = 1; i<= m; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(p[i-1] == s[j-1] || p[i-1] == '?') dp[i][j] = dp[i-1][j-1];
+                else if(p[i-1] == '*')
+                {
+                    dp[i][j] = dp[i][j-1] | dp[i-1][j]; 
+                }
+                else dp[i][j] = false;
+            }
+        }
+
+        return dp[m][n];
     }
 };
