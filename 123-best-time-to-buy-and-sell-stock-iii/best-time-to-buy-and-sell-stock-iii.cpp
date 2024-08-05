1adb5f28d610407d4ee9ae58708481fcd074bce1
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-05 18:07:31 +0530

    Time: 420 ms (54.23%) | Memory: 209.9 MB (43.91%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,32 +1,50 @@
 class Solution {
 public:
-    int f(int idx, int buy, int cap, int& n, vector<int>& prices, vector<vector<vector<int>>>& dp)
-    {
-        if(cap == 2) return 0;
-        if(idx == n) return 0;
-        if(dp[idx][buy][cap] != -1) return dp[idx][buy][cap];
-
-        int profit = 0;
-
-        if(buy)
-        {
-            int Buythis = -prices[idx] + f(idx+1, 0, cap,n, prices,dp); // cap not incremented as curr transaction has not yet been completed
-            int notBuythis = 0 + f(idx+1, 1, cap,n, prices,dp);
-            profit = max(Buythis, notBuythis);
-        }
-        else
-        {
-            int sellthis = prices[idx] + f(idx+1, 1, cap+1,n, prices,dp); // transaction completed here
-            int notsellthis = 0 + f(idx+1, 0, cap,n, prices,dp);
-            profit = max(sellthis, notsellthis);
-        }
-
-        return dp[idx][buy][cap] = profit;
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3, -1)));
 
-        return f(0, 1, 0, n, prices, dp);
+        for(int i = 0; i <= n; i++)
+        {
+            dp[i][0][2] = 0;
+            dp[i][1][2] = 0;
+        }
+
+        for(int b = 0; b<=1; b++)
+        {
+            for(int c = 0; c <= 2; c++)
+            {
+                dp[n][b][c] = 0;
+            }
+        }
+
+
+        for(int idx = n-1; idx >= 0; idx--)
+        {
+            for(int buy = 0; buy <= 1; buy++)
+            {
+                for(int cap = 1; cap >= 0; cap--)
+                {
+                    int profit = 0;
+
+        if(buy)
+        {
+            int Buythis = -prices[idx] + dp[idx+1][0][cap]; 
+            int notBuythis = 0 + dp[idx+1][1][cap];
+            profit = max(Buythis, notBuythis);
+        }
+        else
+        {
+            int sellthis = prices[idx] + dp[idx+1][1][cap+1];
+            int notsellthis = 0 + dp[idx+1][0][cap];
+            profit = max(sellthis, notsellthis);
+        }
+
+        dp[idx][buy][cap] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][0];
     }
 };
