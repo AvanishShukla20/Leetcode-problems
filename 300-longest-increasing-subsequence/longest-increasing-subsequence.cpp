dbc9834966964efdf2a95d6205452145f94773d2
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-09 00:30:50 +0530

    Time: 132 ms (46.66%) | Memory: 13.2 MB (57.68%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    int solve(int idx, int prev, vector<int>& nums, vector<vector<int>>& dp)
-    {
-        int n = nums.size();
-        if(idx == n) return 0;
-        if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
-
-        int t1 = 0, t2 = 0, t3 = 0;
-        if( prev == -1 || nums[idx] > nums[prev])
-        {
-            //take 
-            t1 = solve(idx+1, idx, nums, dp) + 1;
-            //not_take
-            t2 = solve(idx+1, prev, nums, dp);
-        }
-
-        t3 = solve(idx+1, prev, nums, dp);
-        return dp[idx][prev + 1] = max(t1, max(t2, t3));
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int> (n+1, -1));
-        return solve(0, -1, nums, dp);
+        vector<int> dp(n, 1); /* dp[i] represents longest increasing subsequence uptill this idx i */
+        int maxi = 1;
+        for(int i = 0; i< n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(nums[j] < nums[i] && dp[i] < dp[j] + 1)  dp[i] = dp[j] + 1;
+                maxi = max(maxi, dp[i]);
+            }
+        }
+
+        return maxi;
     }
 };
