9091346525912fd2b1895538e50f33fe9ac4e484
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-10 15:36:57 +0530

    Time: 109 ms (77.77%) | Memory: 12.2 MB (41.68%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
-        vector<vector<int>> dp(n+2, vector<int> (n+2, 0));
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
 
         for(int i = n; i >= 1; i--)
         {
@@ -14,7 +14,9 @@ public:
                 for(int par = i; par <= j; par++)
                 {
                     // you must increase size of your dp array to n+2 in tabulation as par may require access of idx n+1
-                    int coins = nums[i-1]*nums[par]*nums[j+1] + dp[i][par-1] + dp[par+1][j];
+                    int coins = nums[i-1]*nums[par]*nums[j+1];
+                    if(par > 1) coins += dp[i][par-1];
+                    if(par < n ) coins += dp[par+1][j];
                     maxi = max(coins, maxi);
                 }
                 dp[i][j] = maxi;
