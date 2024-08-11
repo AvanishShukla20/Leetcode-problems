fc481c913e00844bce40e2cd0734574f0fd72270
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-12 01:17:38 +0530

    Time: 8 ms (74.50%) | Memory: 14.7 MB (19.02%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,67 +1,53 @@
 class Solution {
 public:
-    int drow[4] = {-1, 0, 1, 0};
-    int dcol[4] = {0, -1, 0, 1};
-    int n,m;
-    bool isValid(int row, int col, int n, int m)
-    {
-        return (row >= 0 && row < n && col >= 0 && col < m);
-    }
+    vector<int> drow = {-1, 0, 1, 0};
+    vector<int> dcol = {0, -1, 0, 1};
 
-    void dfs(int row, int col,vector<vector<char>>& board, vector<vector<int>>& vis)
+    void dfs(int r, int c, vector<vector<char>>& board, vector<vector<int>>& vis, vector<vector<int>>& replica)
     {
-        vis[row][col] = 1;
-
-        for(int i=0; i<4; i++)
+        int m = board.size(), n = board[0].size();
+        vis[r][c] = 1;
+        replica[r][c] = 1;
+        for(int i = 0; i<4; i++)
         {
-            int nr = row + drow[i];
-            int nc = col + dcol[i];
-
-            if(isValid(nr, nc, n, m) && !vis[nr][nc] && board[nr][nc] == 'O')
+            int nrow = r + drow[i];
+            int ncol = c + dcol[i];
+            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && board[nrow][ncol] == 'O' && !vis[nrow][ncol])
             {
-                dfs(nr, nc, board, vis);
+                dfs(nrow, ncol, board, vis, replica);
             }
         }
     }
+
+
     void solve(vector<vector<char>>& board) {
-       n = board.size();
-       m = board[0].size(); 
-       vector<vector<int>> vis(n, vector<int>(m, 0));
+        int m = board.size(), n = board[0].size();
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        vector<vector<int>> replica(m, vector<int>(n, 0));
 
-       //along row 
-       for(int i=0; i<m; i++)
-       {
-            if(!vis[0][i] && board[0][i] == 'O')
-            {
-                dfs(0, i, board, vis);
-            }
-            if(!vis[n-1][i] && board[n-1][i] == 'O')
-            {
-                dfs(n-1, i, board, vis);
-            }
-       }
-
-       for(int i=0; i<n; i++)
-       {
-            if(!vis[i][0] && board[i][0] == 'O')
-            {
-                dfs(i, 0, board, vis);
-            }
-            if(!vis[i][m-1] && board[i][m-1] == 'O')
-            {
-                dfs(i, m-1, board, vis);
-            }
-       }
-
-       for(int i= 0; i< n; i++)
-       {
-        for(int j = 0; j < m; j++)
+        //along row
+        for(int i = 0; i < n; i++)
         {
-            if(board[i][j] == 'O' &&  vis[i][j] == 0)
+            if(board[0][i] == 'O' && !vis[0][i]) dfs(0, i, board, vis, replica);
+            if(board[m-1][i] == 'O' && !vis[m-1][i]) dfs(m-1, i, board, vis, replica);
+        }
+        // along column ->
+        for(int i = 0; i < m; i++)
+        {
+            if(board[i][0] == 'O' && !vis[i][0]) dfs(i, 0, board, vis, replica);
+            if(board[i][n-1] == 'O' && !vis[i][n-1]) dfs(i, n-1, board, vis, replica);
+        }
+
+        for(int row = 0; row < m; row++)
+        {
+            for(int col = 0; col < n; col++)
             {
-                board[i][j] = 'X';
+                if(board[row][col] == 'O' && replica[row][col] == 0)
+                {
+                    board[row][col] = 'X';
+                }
             }
         }
-       }
+
     }
 };
