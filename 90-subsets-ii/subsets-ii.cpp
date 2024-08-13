7ac6f0f9ed818619a678bd05ee96ef3485ed2881
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-13 23:47:59 +0530

    Time: 4 ms (48.21%) | Memory: 8.8 MB (75.25%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-
-    void Subsets(int i, vector<int>& nums, vector<int>& temp, set<vector<int>>& s)
+    void solve(int idx, vector<int>& temp, vector<vector<int>>& ans, vector<int>& nums)
     {
-        if(i == nums.size())
+        // Ques -> 2 2 2 3
+        if(idx == nums.size())
         {
-            s.insert(temp);
+            ans.push_back(temp);
             return;
         }
-        //pick
-        temp.push_back(nums[i]);
-        Subsets(i+1,nums,temp,s);
+        // use 2 and find for 2 2 3
+        temp.push_back(nums[idx]);
+        solve(idx+1, temp, ans, nums);
+        // remove 2
         temp.pop_back();
-        
-        // non - pick
-        Subsets(i+1,nums,temp,s);
+
+        //increment idx until you get different element
+        while(idx < nums.size() - 1 && nums[idx] == nums[idx+1]) idx++;
+
+        // 3 is different so call for it
+        solve(idx + 1,temp, ans, nums);
+
     }
-
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
-     sort(nums.begin(), nums.end());
-     
-     vector<int> ds;
-     set<vector<int>> s;
-     Subsets(0,nums,ds,s);
-     vector<vector<int>> ans(s.begin(), s.end());
-     return ans;   
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(nums.begin(), nums.end());
+        solve(0, temp, ans, nums);
+        return ans;
     }
 };
