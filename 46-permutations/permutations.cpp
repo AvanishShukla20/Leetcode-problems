3ff42134ff31e736db69ff1cd50c928b1bf9470a
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-15 23:07:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (28.76%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    void solve(int idx,vector<int>& nums,vector<vector<int>>& ans)
+    void solve(int idx,vector<int>& nums, vector<int>& temp, vector<vector<int>>& ans, unordered_set<int>& st)
     {
+        // the idx here is basically keeping the count of that no of steps done are == nums.size() when reach end
        if(idx == nums.size())
        {
-        ans.push_back(nums);
+            ans.push_back(temp);
+            return;
        }
 
-       for(int i = idx; i < nums.size(); i++)
+       for(int i = 0; i< nums.size(); i++)
        {
-        swap(nums[i], nums[idx]);
-        solve(idx+1, nums, ans);
-        swap(nums[idx], nums[i]);
-       }
+        // notice that poore for loop me current element i.e nums[idx] ka koi significance nhi hai :O
 
+        if(st.find(nums[i]) == st.end())
+        {
+            temp.push_back(nums[i]);
+            st.insert(nums[i]);
+            solve(idx+1, nums, temp, ans, st);
+            temp.pop_back();
+            st.erase(nums[i]);
+        }
+       }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        solve(0, nums,ans);
+        vector<int> temp;
+        unordered_set<int> st;
+        solve(0, nums, temp, ans, st);
         return ans;
     }
 };
