90eb865402c84ad4a32b341c6ad3686fd31fd9c1
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-19 12:25:58 +0530

    Time: 112 ms (48.31%) | Memory: 108.1 MB (27.70%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
-    void solve(int idx, vector<int>&nums, vector<int>& temp, vector<vector<int>>& ans,unordered_set<int>& st, int k)
+    void solve(int idx, vector<int>&temp,vector<vector<int>>& ans,int k, int n)
     {
         if(k == 0)
         {
             ans.push_back(temp);
             return;
         }
-        if(idx == nums.size()) return;
+        if(idx == n + 1) return;
+        //take and explore
+        temp.push_back(idx);
+        solve(idx+1,temp, ans, k-1, n);
+        temp.pop_back();
 
-        for(int i = idx+1; i<nums.size(); i++)
-        {
-            if(st.find(nums[i]) == st.end())
-            {
-                temp.push_back(nums[i]);
-                st.insert(nums[i]);
-                solve(i, nums, temp,ans, st, k-1);
-                temp.pop_back();
-                st.erase(nums[i]);
-            }
-        }
+        solve(idx+1,temp, ans, k, n);
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int> nums(n);
         vector<vector<int>> ans;
         unordered_set<int> st;
         vector<int> temp;
-        for(int i = 0; i<n; i++) nums[i] = i + 1;
-        for(int i = 0; i<n; i++)
-        {
-            temp.push_back(nums[i]);
-            st.insert(nums[i]);
-            solve(i, nums, temp, ans, st, k-1);
-            temp.pop_back();
-            st.erase(nums[i]);
-        }
+
+        solve(1,temp, ans, k, n);
 
         return ans;
 
