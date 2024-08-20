217888251598314c01927a582b65baf4ee284e33
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-20 20:39:24 +0530

    Time: 42 ms (35.81%) | Memory: 14.1 MB (35.17%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    int nthUglyNumber(int n) {
-        vector<int> ans(n+1); // ans[i] will store my ith ugly no
-        ans[1] = 1;
-
-        int i2 = 1, i3 = 1, i5 = 1;
-
-        for(int i = 2; i<= n; i++)
+    int solve(int k)
+    {
+        priority_queue<long long , vector<long long>, greater<long long>> pq;
+        pq.push(1);
+        long long cnt = 0, ans = 0;
+        while(!pq.empty() && cnt < k)
         {
-            int temp2 = 2*ans[i2];
-            int temp3 = 3*ans[i3];
-            int temp5 = 5*ans[i5];
-
-            int ithUgly = min(temp2, min(temp3, temp5));
-            ans[i] = ithUgly;
-
-            if(ithUgly == temp2)
-            {
-                // i2 ko badhayenge kyonki ye iteration me uske index ka element to use hogya . So, next iteration ke liye use doore idx ko pint krna hoga jo valid ans de ske
-                i2++;
-            }
-            if(ithUgly == temp3) i3++;
-            if(ithUgly == temp5) i5++ ;
-
+            long long num = pq.top();
+            pq.pop();
+            if(ans == num) continue;
+            ans = num;
+            cnt++;
+            pq.push(num*2);
+            pq.push(num*3);
+            pq.push(num*5);
         }
 
-        return ans[n];
-
+        return ans;
+    }
+    int nthUglyNumber(int n) {
+        return solve(n);
     }
 };
