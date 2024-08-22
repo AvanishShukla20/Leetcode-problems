3765f0b5eb735c1f61e5e28edf4543241ece5b51
Avanish Shukla <avanishshukla2021@gmail.com>
2024-08-23 02:03:27 +0530

    Time: 3 ms (41.26%) | Memory: 7.6 MB (34.03%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -9,7 +9,7 @@ public:
         }
 
         int cnt = 0;
-        for(int i = 0; i<31; i++)
+        for(int i = 0; i<=31; i++)
         {
             if(cnt > k) break;
             num = num ^ (1 << i);
