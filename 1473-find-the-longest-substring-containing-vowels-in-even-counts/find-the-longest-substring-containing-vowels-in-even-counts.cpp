39441ada85481683d4efd8c6da9b407999dda458
Avanish Shukla <avanishshukla2021@gmail.com>
2024-09-16 10:13:46 +0530

    Time: 82 ms (40.60%) | Memory: 19.1 MB (13.07%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -3,42 +3,36 @@ public:
     int findTheLongestSubstring(string s) {
         int hash[5] = {0};
         int n = s.size();
-        unordered_map<string, int> mpp;
-        mpp["00000"] = -1;
+        unordered_map<int, int> mpp;
+        mpp[0] = -1;
+        int mask = 0, state = 0;
         int res = 0;
 
         for(int i=0; i<n; i++)
         {
             if(s[i] == 'a')
             {
-                hash[0]++;
-                hash[0] %= 2;
+                mask ^= (state|(1));
             }
             if(s[i] == 'e')
             {
-                hash[1]++;
-                hash[1] %= 2;
+                mask ^= (state|(1<<1));
             }
             if(s[i] == 'i')
             {
-                hash[2]++;
-                hash[2] %= 2;
+                mask ^= (state|(1<<2));
             }
             if(s[i] == 'o')
             {
-                hash[3]++;
-                hash[3] %= 2;
+                mask ^= (state|(1<<3));
             }
             if(s[i] == 'u')
             {
-                hash[4]++;
-                hash[4] %= 2;
+                mask ^= (state|(1<<4));
             }
-            string t = "";
-            for(int i=0; i<5;i++) t += to_string(hash[i]);
             
-            if(mpp.find(t) != mpp.end()) res = max(res, i - mpp[t]);
-            else mpp[t] = i;
+            if(mpp.find(mask) != mpp.end()) res = max(res, i - mpp[mask]);
+            else mpp[mask] = i;
         }
 
         return res;
