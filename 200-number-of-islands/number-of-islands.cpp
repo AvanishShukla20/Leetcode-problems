1ac8340ed5bf31ea6ceb7e90eba90552e9ba9740
Avanish Shukla <avanishshukla2021@gmail.com>
2024-09-21 00:51:01 +0530

    Time: 36 ms (22.40%) | Memory: 24.5 MB (13.84%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,92 +1,57 @@
-class DisjointSet
-{
-    public:
-    vector<int> parent, size;
-    DisjointSet(int n)
-    {
-        parent.resize(n+1);
-        size.resize(n+1, 1);
-        for(int i = 0; i<= n; i++) parent[i] = i;
-    }
-
-    int find(int a)
-    {
-        if(parent[a] == a) return a;
-
-        return parent[a] = find(parent[a]);
-    }
-
-    void Union(int a, int b)
-    {
-        int ulpa = find(a);
-        int ulpb = find(b);
-
-        if(ulpa == ulpb) return;
-
-        if(size[ulpa] < size[ulpb])
-        {
-            parent[ulpb] = ulpa;
-            size[ulpb] += size[ulpa];
-        }
-        else
-        {
-            parent[ulpa] = ulpb;
-            size[ulpa] += size[ulpb];
-        }
-    }
-};
-
- 
 class Solution {
 public:
-    void bfs(int& ir, int& ic, vector<vector<char>>& grid, vector<vector<int>>& vis)
+    void bfs(int row, int col, vector<vector<int>>& isVisited, vector<vector<char>>& grid)
     {
-        vis[ir][ic] = 1;
-        int m = grid.size(),n = grid[0].size();
+        isVisited[row][col] = 1;
+        queue<pair<int, int>> Q;
+        Q.push({row, col});
 
-        vector<int> dr = {-1, 0, 1, 0};
-        vector<int> dc = {0, -1, 0, 1};
+        vector<pair<int,int>> directions{{0,1},{0,-1},{1,0},{-1,0}};
 
-        queue<pair<int, int>> q;
-        q.push({ir, ic});
-
-        while(!q.empty())
+        int n = grid.size();
+        int m = grid[0].size();
+        while(!Q.empty())
         {
-            auto tuple = q.front();
-            q.pop();
-            int i = tuple.first;
-            int j = tuple.second;
+            auto x = Q.front();
+            int r = x.first;
+            int c = x.second;
+            Q.pop();
 
-            for(int c = 0; c < 4; c++)
+            for(int del= 0; del < directions.size(); del++)
             {
-                int drow = i + dr[c];
-                int dcol = j + dc[c];
+                    int newRow = r + directions[del].first;
+                    int newCol = c + directions[del].second;
 
-                if(drow >= 0 && drow < m && dcol >= 0 && dcol < n && grid[drow][dcol] == '1' && !vis[drow][dcol])
-                {
-                    vis[drow][dcol] = 1;
-                    q.push({drow, dcol});
-                }
+                    if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == '1' && isVisited[newRow][newCol] == 0 )
+                    {
+                        isVisited[newRow][newCol] = 1;
+                        Q.push({newRow, newCol});
+                    }
+                
             }
         }
+
     }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(),n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        int cnt = 0;
-        for(int i = 0; i<m; i++)
+        int n = grid.size();
+        int m = grid[0].size();
+
+        
+        vector<vector<int>> isVisited(n, vector<int>(m , 0));
+        int count = 0;
+
+        for(int i = 0; i<n; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j<m;j++)
             {
-                if(!vis[i][j] && grid[i][j] == '1')
+                if(grid[i][j] == '1' && isVisited[i][j] == 0)
                 {
-                    bfs(i,j, grid, vis);
-                    cnt += 1;
+                    count++;
+                    bfs(i, j, isVisited, grid);
                 }
             }
-            
         }
+        return count;
 
-        return cnt;
     }
 };
