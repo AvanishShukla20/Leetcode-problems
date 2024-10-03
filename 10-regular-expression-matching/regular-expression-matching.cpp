d9f85dd88934d371f91356167ae2f45f53d18354
Avanish Shukla <avanishshukla2021@gmail.com>
2024-10-04 01:04:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (56.23%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
+    int dp[23][23];
     bool solve(int i, int j, string&s, string& p)
     {
         if(j == p.size())
         {
             return (i == s.size());
         }
+        
+        if(dp[i][j] != -1) return dp[i][j];
 
         //check if current is matching or not
         bool first_match = (i < s.size() && (s[i] == p[j] || p[j] == '.')) ? 1 : 0;
@@ -19,14 +22,15 @@ public:
             }
             bool notTake_asterisk = solve(i, j+2, s, p);
 
-            return take_asterisk || notTake_asterisk ;
+            return dp[i][j] = take_asterisk || notTake_asterisk ;
         }
         else
         {
-            return first_match && solve(i+1, j+1, s, p);
+            return dp[i][j] = first_match && solve(i+1, j+1, s, p);
         }
     }
     bool isMatch(string s, string p) {
+        memset(dp, -1, sizeof(dp));
         return solve(0, 0, s, p);
     }
 };
