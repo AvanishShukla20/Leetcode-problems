77d6450259cd75f70c0a01c00e8106f83335cddc
Avanish Shukla <avanishshukla2021@gmail.com>
2024-10-03 16:44:47 +0530

    Time: 1216 ms (6.74%) | Memory: 8.3 MB (92.91%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool ispalindrome(int i, int j, string&s)
+    {
+        while(i <= j)
+        {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        int n = s.size();
+        string ans = "";
+        int maxi = 0;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=n-1; j>=i; j--)
+            {
+                if(ispalindrome(i, j, s))
+                {
+                    if(j-i+1 > maxi)
+                    {
+                        ans = s.substr(i, j-i+1);
+                        maxi = j - i + 1;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
