c6b54be31e2101d5fa2c1ad3b160e304e83a9c7c
Avanish Shukla <avanishshukla2021@gmail.com>
2024-10-04 00:59:46 +0530

    Time: 446 ms (5.03%) | Memory: 7.8 MB (96.49%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-bool solve(string &s,string &p,int i,int j,vector<vector<int>>&dp){
-// base condition if  we pass whole p string and check string s is also passed or not 
-    if (j == p.length()) {
-            return i == s.length();
+    bool solve(int i, int j, string&s, string& p)
+    {
+        if(j == p.size())
+        {
+            return (i == s.size());
         }
 
-    
-     // if the i,j is  already computed than skip to find
-    if(dp[i][j]!=-1){
-        return dp[i][j];
+        //check if current is matching or not
+        bool first_match = (i < s.size() && (s[i] == p[j] || p[j] == '.')) ? 1 : 0;
+
+        bool take_asterisk = false;
+        if(j+1 < p.size() && p[j+1] == '*')
+        {
+            if(first_match == true)
+            {
+                take_asterisk = solve(i+1, j, s, p);
+            }
+            bool notTake_asterisk = solve(i, j+2, s, p);
+
+            return take_asterisk || notTake_asterisk ;
+        }
+        else
+        {
+            return first_match && solve(i+1, j+1, s, p);
+        }
     }
-
-    // find the character at i index of string s anf the character at j index aat string p is equal or p[j]='.' than firstMatch is true
-    bool firstMatch = (i < s.length() && (s[i] == p[j] || p[j] == '.'));
-// check for the use of '*' if '*' is use for skip or how many seqence repetedly
-//store the value in dp 
-  if (j + 1 < p.length() && p[j + 1] == '*') {
-           
-            dp[i][j] = solve(s, p, i, j + 2, dp) || (firstMatch && solve(s, p, i + 1, j, dp));
-        } else {
-          
-            dp[i][j] = firstMatch && solve(s, p, i + 1, j + 1, dp);
-        }
-//return dp[i][j]
-    return dp[i][j];
-}
     bool isMatch(string s, string p) {
-       //intialize dp vector with length+1 
-        vector<vector<int>>v(s.length()+1,vector<int>(p.length()+1,-1));
-//call the function
-        return solve(s,p,0,0,v);
-            
+        return solve(0, 0, s, p);
     }
 };
