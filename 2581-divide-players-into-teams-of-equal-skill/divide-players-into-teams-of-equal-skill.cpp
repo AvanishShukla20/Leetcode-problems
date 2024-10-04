bf76dd59ccc3636a63ccba64e5180098118a2b22
Avanish Shukla <avanishshukla2021@gmail.com>
2024-10-04 11:25:57 +0530

    Time: 58 ms (82.80%) | Memory: 55.9 MB (98.40%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -2,22 +2,30 @@ class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
         int n = skill.size();
-        sort(skill.begin(), skill.end());
-        int i = 0, j = n-1;
-        int sum = skill[0] + skill[n-1];
-        long long ans = 0;
+        int totalSum = 0;
+        vector<int> freq(1001, 0);
 
-        while(i < j)
+        for(int i=0; i<n; i++)
         {
-            if(skill[i] + skill[j] == sum)
-            {
-                ans = ans + (1LL * skill[i] * skill[j]);
-            }
-            else return -1;
-            i++;
-            j--;
+            freq[skill[i]]++;
+            totalSum += skill[i];
+        }
+        if(totalSum % (n/2) != 0) return -1;
+
+        int targetSum = totalSum/(n/2);
+        long long sum = 0;
+        int noOfPair = 0;
+        
+
+        for(int currskill : skill) // i am iterating in skill array not in freq array
+        {
+            int partner = targetSum - currskill;
+            if(freq[partner] == 0) return -1;
+
+            sum += 1LL * currskill * partner;
+            freq[partner]--;
         }
 
-        return ans;
+        return sum/2;
     }
 };
