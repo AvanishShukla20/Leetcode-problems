6e5b3fb4c555475fad74c1018ece95db14c2fd31
Avanish Shukla <avanishshukla2021@gmail.com>
2024-10-05 00:22:41 +0530

    Time: 118 ms (95.60%) | Memory: 74.2 MB (17.10%) - LeetSync

---
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    typedef long long ll;
+    int minSubarray(vector<int>& nums, int p) {
+        int n = nums.size();
+        ll sum = 0, totalsum = 0;
+
+        for(int x : nums) totalsum += x;
+        if(totalsum < p) return -1;
+        ll targetrem = totalsum%p;
+        if(targetrem == 0) return 0;
+
+        // we need to find min subarray len such that sum of subarray equals this targetrem
+        ll prefix = 0;
+        unordered_map<ll, int> mpp;
+        mpp[0] = -1;
+        int mini = n;
+        for(int i = 0; i<n; i++)
+        {
+            prefix += nums[i];
+            prefix %= p;
+
+            int lookfor = (prefix - targetrem + p) % p;
+
+            if(mpp.find(lookfor) != mpp.end())
+            {
+                mini = min(mini, i - mpp[lookfor]);
+            }
+            mpp[prefix] = i;
+        }
+        return (mini == n) ? -1 : mini;
+    }
+};
