1789eed3b267443ff0eab3d64ff2e29d6865f663
Avanish Shukla <avanishshukla2021@gmail.com>
2024-10-24 03:50:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (27.28%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.cpp
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -2,80 +2,47 @@ class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
         priority_queue<pair<int, char>> pq;
+        if (a > 0) pq.push({a, 'a'});
+        if (b > 0) pq.push({b, 'b'});
+        if (c > 0) pq.push({c, 'c'});
 
-        if(a >0) pq.push({a, 'a'});
-        if(b>0) pq.push({b, 'b'});
-        if(c>0) pq.push({c, 'c'});
-        string str = "";
-        char flag ='#';
-        char prev_ch;
-        int prev_freq;
-        while(pq.size() > 1)
-        {
-            auto it1 = pq.top();
-            pq.pop();
-            auto it2 = pq.top();
-            pq.pop();
+        string result = "";
 
-            int fc1 = it1.first, fc2 = it2.first;
-            char ch1 = it1.second, ch2 = it2.second;
-
-            if(fc1 >= 2)
-            {
-                if(prev_ch != ch1)
-                {
-                    fc1 -= 2;
-                    flag = ch1;
-                    str += string(2, ch1);
-                    prev_ch = ch1;
-                    prev_freq = 2;
-                }
-            }
-            else if(fc1 == 1)
-            {
-                if(prev_ch == ch1)
-                {
-                    if(prev_freq < 2)
-                    {
-                        fc1 -= 1;
-                        str += ch1;
-                        prev_ch = ch1;
-                        prev_freq = 2;
-                    }
-                }
-                else
-                {
-                    fc1 -= 1;
-                    str += ch1;
-                    prev_ch = ch1;
-                    prev_freq = 1;
-                }
-                
-            }
-
-            if(fc2 >= 1)
-            {
-                fc2 -= 1;
-                str += ch2;
-                prev_ch = ch2;
-                prev_freq = 1;
-            }
-
-            if(fc1 != 0) pq.push({fc1, ch1});
-            if(fc2 != 0) pq.push({fc2, ch2});
-        }
-
-        if(!pq.empty())
+        while(!pq.empty())
         {
             auto it = pq.top();
             pq.pop();
-            char ch = it.second;
-            int freq = it.first;
-            if(freq == 1) str += ch;
-            else str += string(2, ch);
+            int freq1 = it.first;
+            char ch1 = it.second;
 
+            //check if the charcter ch1 is already used two times or not
+            if(result.size() >= 2 && result[result.size() - 1]== ch1 && result[result.size() - 2] == ch1)
+            {
+                //proceed to add next avilable character
+                
+                if(pq.empty())
+                {
+                    //if pq is empty then break from here and return result
+                    break;
+                }
+
+                int freq2 = pq.top().first;
+                char ch2 = pq.top().second;
+                pq.pop();
+                result += ch2;
+                freq2 -= 1;
+                if(freq2 > 0) pq.push({freq2, ch2});
+
+                if(freq1 > 0) pq.push({freq1, ch1});
+            }
+            else
+            {
+                result += ch1;
+                freq1 -= 1;
+                if(freq1 > 0) pq.push({freq1, ch1});
+            }
+            
         }
-
-        return str;
+        return result;
     }
 };
