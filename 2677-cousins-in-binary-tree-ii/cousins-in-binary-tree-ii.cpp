3199cb3e58510f69e944631173cb3fbdbaf8b238
Avanish Shukla <avanishshukla2021@gmail.com>
2024-10-24 15:38:49 +0530

    Time: 77 ms (90.02%) | Memory: 341.7 MB (20.37%) - LeetSync

--- 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.cpp
+++ 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.cpp
@@ -11,22 +11,7 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, int& lvl, unordered_map<TreeNode*, int>& mpp,unordered_map<int , int>& lvlsum)
-    {
-        if(!root) return;
-
-        int sum = lvlsum[lvl];
-        if(root->left) sum -= root->left->val;
-        if(root->right) sum -= root->right->val;
-        mpp[root->left] = sum;
-        mpp[root->right] = sum;
-
-        lvl += 1;
-        if(root->left) dfs(root->left,lvl, mpp, lvlsum);
-        if(root->right) dfs(root->right,lvl , mpp, lvlsum);
-        lvl -= 1;
-
-    }
+    
     TreeNode* replaceValueInTree(TreeNode* root) {
         unordered_map<int , int> lvlsum;
         
@@ -43,13 +28,38 @@ public:
             if(node->left) q.push({lvl+1, node->left});
             if(node->right) q.push({lvl+1, node->right});
         }
-        unordered_map<TreeNode*, int> mpp;
-        int lvl = 1;
-        dfs(root, lvl, mpp,lvlsum);
 
-        for(auto it : mpp)
+        //another bfs 
+        // It uses size of queue at each level to determine no of nodes at each level.
+
+        queue<TreeNode*> q2;
+        q2.push(root);
+        int lvl = 1;
+        while(!q2.empty())
         {
-            if(it.first) it.first->val = it.second;
+            int nodesAtlvl = q2.size();
+
+            for(int i=0; i<nodesAtlvl; i++)
+            {
+                auto node = q2.front();
+                q2.pop();
+                int siblingSum = 0;
+                if(node->left) siblingSum += node->left->val;
+                if(node->right) siblingSum += node->right->val;
+
+                if(node->left)
+                {
+                    node->left->val = lvlsum[lvl] - siblingSum;
+                    q2.push(node->left);
+                }
+                if(node->right)
+                {
+                    node->right->val = lvlsum[lvl] - siblingSum;
+                    q2.push(node->right);
+                }
+
+            }
+            lvl++;
         }
         root->val = 0;
         return root;
