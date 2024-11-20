a8450d364c1991433ba9af9cd0d767810d45574a
Avanish Shukla <avanishshukla2021@gmail.com>
2024-11-20 14:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 69.6 MB (17.91%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,51 +1,29 @@
 class Solution {
 public:
-    int binsearch(int val, int l, int h, vector<int>&nums)
-    {
-        int n = nums.size(), ans = n, mid;
-        while(l <= h)
-        {
-            mid = l + (h-l)/2;
-            if(nums[mid]>= val)
-            {
-                ans = mid;
-                h = mid-1;
-            }
-            else l = mid+1;
-        }
-        return ans;
-    }
     int findLengthOfShortestSubarray(vector<int>& arr) {
-        int n=arr.size();
+        int n = arr.size();
+        int j=n-1;
+        while(j> 0 && arr[j-1] <= arr[j]) j--;
 
-        int pr = -1,sf = n;
-        for(int i=0;i<n-1;i++)
+        int i=0,ans = j;
+        while(j<n && i<j)
         {
-            if(arr[i]<=arr[i+1]) pr = i;
-            else break;
-        }
-        pr++;
-        if(pr == n-1) return 0; 
-
-        for(int i=n-1;i>=1;i--)
-        {
-            if(arr[i-1]<=arr[i])
+            if(arr[i] <= arr[j])
             {
-                sf = i-1;
+                ans = min(ans, j-i-1);
+                if(arr[i] > arr[i+1]) break;
+                else i++;
             }
-            else break;
+            else j++;
         }
-        if(sf == n) sf--;
-
-        cout<<"pr : "<<pr<<"sf : "<<sf<<endl;
-        int len = sf;
-        for(int k = 0; k<=pr; k++)
+        ans = min(ans, j-i-1);
+        //if j ended but some sorted part can be more covered by idx i
+        while(i< n-2 && arr[i]<=arr[i+1]) 
         {
-            int find_in_suffix = binsearch(arr[k], sf, n-1, arr);
-            cout<<find_in_suffix<<" ";
-            len = min(len, abs(find_in_suffix - k - 1));
+            i++;
+            ans = min(ans, j-i-1);
         }
 
-        return len;
+        return ans <= 0 ? 0 : ans;
     }
 };
