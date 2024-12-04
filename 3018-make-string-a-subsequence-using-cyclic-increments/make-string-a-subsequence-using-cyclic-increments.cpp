8ff2de7d6697fd59a7b5b2511b6e56e6a64df6a9
Avanish Shukla <avanishshukla2021@gmail.com>
2024-12-04 22:41:49 +0530

    Time: 8 ms (26.52%) | Memory: 17.2 MB (7.53%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,49 +1,22 @@
 class Solution {
 public:
-    bool solve(int i, int j,int& cnt,string&str1, string&str2)
-    {
-        if(j == str2.size()) return true;
-        if(i == str1.size())
+    bool canMakeSubsequence(string str1, string str2) {
+        int n = str1.size(), m= str2.size();
+        int i =0, j = 0;
+        while(j < m && i < n)
         {
-            if(j >= str2.size()) return true;
-            return false;
+            if( str1[i]-'a' == str2[j]-'a' || (str1[i] + 1-'a')%26 == (str2[j]-'a')%26)
+            {
+                i++;
+                j++;
+            }
+            else 
+            {
+                i++;
+            }
         }
 
-        bool ans = false;
-        if(cnt == 1)
-        {
-            if(str1[i] -'a' == str2[j] -'a' || (str1[i]+1 -'a')%26 == (str2[j] -'a'))
-            {
-                ans = true && solve(i+1, j+1, cnt, str1, str2);
-            }  
-            else
-            {
-                ans = false || solve(i+1, j, cnt, str1, str2);
-            } 
-        }
-        else
-        {
-            if(str1[i] -'a' == str2[j] -'a')
-            {
-                ans = true && solve(i+1, j+1, cnt, str1, str2);
-            }
-            else if((str1[i]+1 -'a')%26 == (str2[j] -'a')) 
-            {
-                cnt += 1;
-                ans = true && solve(i+1, j+1, cnt, str1, str2);
-                cnt -= 1;
-            }
-            else
-            {
-                ans = false || solve(i+1, j, cnt, str1, str2);
-            }
-        }
-        return ans;
-    }
-    bool canMakeSubsequence(string str1, string str2) {
-        int n = str1.size(), m = str2.size();
-        if(n < m) return false;
-        int i=0,j=0, cnt = 0;
-        return solve(i, j,cnt, str1, str2);
+        if(j == m) return true;
+        return false;
     }
 };
