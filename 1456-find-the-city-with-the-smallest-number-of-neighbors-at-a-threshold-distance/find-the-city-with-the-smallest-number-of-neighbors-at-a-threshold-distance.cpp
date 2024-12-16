116d8fad13edab43aedaee019890f088aeeff363
Avanish Shukla <avanishshukla2021@gmail.com>
2024-12-16 11:54:12 +0530

    Time: 214 ms (5.05%) | Memory: 20.6 MB (11.98%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,74 +1,79 @@
 class Solution {
 public:
-    void dijkstra(int src, int n, vector<vector<pair<int, int>>>& adj, vector<int>& distance)
-    {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({src, 0});
-        distance[src] = 0;
-
-        while(!pq.empty())
-        {
-            int node = pq.top().first;
-            int dst = pq.top().second;
-            pq.pop();
-
-            for(auto &it : adj[node])
-            {
-                int neigh = it.first;
-                int ds = it.second;
-                if(dst + ds < distance[neigh])
-                {
-                    distance[neigh] = dst + ds;
-                    pq.push({neigh, dst + ds});
-                } 
-            }
-        }
-
-    }
-
+    #define P pair<int, int> 
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<pair<int, int>>> adj(n);
 
-        for(int i = 0; i<edges.size(); i++)
+        vector<vector<P>> adj(n);
+
+        for(int i=0; i<edges.size(); i++)
         {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int w = edges[i][2];
-
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
+            adj[edges[i][0]].push_back({edges[i][1], edges[i][2]});
+            adj[edges[i][1]].push_back({edges[i][0], edges[i][2]});
         }
 
+        priority_queue<P, vector<P>, greater<P>> pq1;
 
-        int ans = -1e9;
-        int minFoundTillNow = 1e6;
-
-        for(int i = 0; i < n; i++)
+        for(int i=0; i<n; i++)
         {
-            vector<int> distance(n, 1e9);
-            
-            dijkstra(i, n, adj,distance);
-            cout<<"i : "<<i<<endl;
-            int count = 0;
+            int start = i;
+            vector<int> dist(n, 1e9);
+            dist[i] = 0;
+            priority_queue<P, vector<P>, greater<P>> pq;
+            pq.push({0, i});
 
-            for(int i = 0; i < distance.size(); i++)
+            while(!pq.empty())
             {
-                cout<<distance[i]<<endl;
-                if(distance[i] <= distanceThreshold)
+                auto t = pq.top();
+                pq.pop();
+                int node = t.second;
+                int d = t.first;
+
+                for(auto &it : adj[node])
                 {
-                    count++;
+                    int adjnode = it.first;
+                    int dis = it.second;
+
+                    if(d+dis <= dist[adjnode])
+                    {
+                        dist[adjnode] = d+dis;
+                        pq.push({dist[adjnode], adjnode});
+                    }
                 }
             }
 
-            
-            if(count <= minFoundTillNow)
-            {
-                minFoundTillNow = count;
-                ans = max(ans, i);
-            }
+            int cnt = 0;
+            cout<<start<<endl;
 
+            for(int i=0; i<dist.size(); i++)
+            {
+                cout<<" "<<i<<" -> "<<dist[i];
+                if(i != start && dist[i]<=distanceThreshold) cnt++;
+            }
+            
+            cout<<endl;
+            pq1.push({cnt, start});
+            
         }
 
+        
+        int ans = 1000;
+        int md = 1e9;
+        while(!pq1.empty())
+        {
+            auto p = pq1.top();
+            pq1.pop();
+            if(ans == 1000)
+            {
+                ans = p.second;
+                md = p.first;
+            }
+            else if(md == p.first)
+            {
+                ans = p.second;
+            }
+            else break;
+        }
+        
         return ans;
     }
 };
