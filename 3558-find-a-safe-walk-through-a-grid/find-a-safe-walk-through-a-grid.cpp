e57c6e45f00608d80d353ba3c105a6cede40ae79
Avanish Shukla <avanishshukla2021@gmail.com>
2024-12-23 10:43:31 +0530

    Time: 23 ms (62.94%) | Memory: 31.5 MB (61.17%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
-    bool solve(int i, int j,int health, vector<vector<int>>& grid, vector<vector<int>>& vis,vector<vector<vector<int>>>& dp)
-    {
-        int n = grid[0].size(), m = grid.size();
-        if (i >= m || j >= n || i<0 || j<0 || health <= 0) return false;
-        
-        if(i == m-1 && j == n-1)
-        {
-            if(health > grid[i][j]) return true;
-            return false;   
-        }
+    #define P pair<int, pair<int, int>> 
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        //dijkstra
+        int m = grid.size(), n= grid[0].size();
+        priority_queue<P, vector<P>, greater<P>> pq;
+        vector<vector<int>> dist(m, vector<int> (n, 1e4));
 
-        if(dp[i][j][health] != -1) return dp[i][j][health];
-
-        vis[i][j] = 1;
-        int drow[4] = {0, 1, 0, -1};
-        int dcol[4] = {1, 0, -1, 0};
-        int s = false;
-        for(int p = 0; p < 4; p++)
+        pq.push({grid[0][0], {0, 0}});
+        vector<vector<int>> traversevec = {{0,1},{1,0},{0,-1},{-1,0}};
+        while(!pq.empty())
         {
-            int nr = i + drow[p];
-            int nc = j + dcol[p];
-            if(nr >= 0 && nc >= 0 && nr < m && nc < n && !vis[nr][nc])
+            auto p = pq.top();
+            pq.pop();
+            int d = p.first;
+            int r = p.second.first;
+            int c = p.second.second;
+
+            for(int i=0; i<traversevec.size(); i++)
             {
-                
-                if(grid[i][j] == 1) s |= solve(nr, nc,health - 1, grid, vis,dp);
-                else s |= solve(nr, nc, health, grid, vis,dp);
+                int nr = traversevec[i][0] + r;
+                int nc = traversevec[i][1] + c;
+
+                if(nr>=0&&nr<m&&nc>=0&&nc<n&& dist[nr][nc] > d + grid[nr][nc])
+                {
+                    dist[nr][nc] = d + grid[nr][nc];
+                    pq.push({d+grid[nr][nc], {nr, nc}});
+                }
             }
         }
 
-        vis[i][j] = 0;
-        return dp[i][j][health] = s;
-
-    }
-    bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int n = grid[0].size(), m = grid.size();
-        
-        vector<vector<vector<int>>> dp(m, vector<vector<int>> (n, vector<int> (health + 2, -1)));
-        vector<vector<int>> vis(m, vector<int> (n, 0));
-
-        return solve(0, 0,health, grid, vis, dp);
+        cout<<dist[m-1][n-1]<<endl;
+        if(dist[m-1][n-1] + 1 <= health) return true;
+        return false;
     }
 };
