a8028a4a1e2b0f0cd32b3c47db92c4d467f55612
Avanish Shukla <avanishshukla2021@gmail.com>
2024-12-24 22:52:23 +0530

    Time: 685 ms (8.21%) | Memory: 245.8 MB (78.79%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
@@ -1,47 +1,96 @@
 class Solution {
 public:
-    vector<int> leftmostBuildingQueries(vector<int>& heights,
-                                        vector<vector<int>>& queries) {
-        vector<pair<int, int>> monoStack;
-        vector<int> result(queries.size(), -1);
-        vector<vector<pair<int, int>>> newQueries(heights.size());
-        for (int i = 0; i < queries.size(); i++) {
-            int a = queries[i][0];
-            int b = queries[i][1];
-            if (a > b) swap(a, b);
-            if (heights[b] > heights[a] || a == b)
-                result[i] = b;
-            else
-                newQueries[b].push_back({heights[a], i});
+    vector<int> segmentTree;
+    void buildSegTree(vector<int>& heights, int start, int end, int node_idx)
+    {
+        // start and end are the start and end of range this node will represent
+        if(start == end)
+        {
+            segmentTree[node_idx] = start;
+            return;
         }
 
-        for (int i = heights.size() - 1; i >= 0; i--) {
-            int monoStackSize = monoStack.size();
-            for (auto& [a, b] : newQueries[i]) {
-                int position = search(a, monoStack);
-                if (position < monoStackSize && position >= 0)
-                    result[b] = monoStack[position].second;
-            }
-            while (!monoStack.empty() && monoStack.back().first <= heights[i])
-                monoStack.pop_back();
-            monoStack.push_back({heights[i], i});
-        }
-        return result;
+        int mid = start + (end-start)/2;
+        int leftChildIdx = 2*node_idx;
+        int rightChildIdx = 2*node_idx + 1;
+        buildSegTree(heights, start, mid, leftChildIdx);
+        buildSegTree(heights, mid+1, end, rightChildIdx);
+
+        segmentTree[node_idx] = heights[segmentTree[leftChildIdx]] >= heights[segmentTree[rightChildIdx]] ? segmentTree[leftChildIdx] : segmentTree[rightChildIdx];
+
     }
 
-private:
-    int search(int height, vector<pair<int, int>>& monoStack) {
-        int left = 0;
-        int right = monoStack.size() - 1;
-        int ans = -1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (monoStack[mid].first > height) {
-                ans = max(ans, mid);
-                left = mid + 1;
-            } else
-                right = mid - 1;
+    int rangeMaxQuery(vector<int>& heights, int reqs, int reqe, int start, int end, int node_idx)
+    {
+        if(reqs <= start && end <= reqe) // total overlap start and end are of given node
+        {
+            return segmentTree[node_idx];
         }
-        return ans;
+        if(reqe < start || end < reqs)
+        {
+            return INT_MIN;
+        }
+
+        int mid = start + (end-start)/2;
+        int leftChildIdx = 2*node_idx;
+        int rightChildIdx = 2*node_idx + 1;
+
+        int left = rangeMaxQuery(heights, reqs, reqe, start, mid, leftChildIdx);
+        int right = rangeMaxQuery(heights, reqs, reqe, mid+1, end, rightChildIdx);
+
+        if(left == INT_MIN) return right;
+        if(right == INT_MIN) return left;
+
+        return heights[left] >= heights[right] ? left : right;
+    }
+    vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
+        int n = heights.size();
+        segmentTree = vector<int> (4*n + 1);
+        int node_idx = 1;
+
+
+        buildSegTree(heights, 0, n-1, node_idx);
+
+        vector<int> result;
+
+        for(auto &query : queries)
+        {
+            int aliceidx = min(query[0], query[1]);
+            int bobidx = max(query[0], query[1]);
+
+            if(aliceidx == bobidx || heights[aliceidx] < heights[bobidx])
+            {
+                result.push_back(bobidx);
+                continue;
+            }
+
+            // perform binary search and RMQ using segment tree for Next greater element
+
+            int low = bobidx;
+            int high = n-1;
+            int ans = INT_MAX;
+            while(low <= high)
+            {
+                int mid = low + (high-low)/2;
+                int rmq = rangeMaxQuery(heights, low, mid, 0, n-1, node_idx);
+
+                if(heights[rmq] > heights[aliceidx])
+                {
+                    ans = min(ans, rmq);
+                    high = mid-1;
+                }
+                else
+                {
+                    low = mid+1;
+                }
+
+            }
+            if(ans == INT_MAX) result.push_back(-1);
+            else result.push_back(ans);
+            
+        }
+        
+        return result;
+        
     }
 };
