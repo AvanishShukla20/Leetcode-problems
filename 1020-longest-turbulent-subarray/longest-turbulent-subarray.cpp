0c9d604d6fb8b7ffba93cb7745a04a5b33b3cc14
Avanish Shukla <avanishshukla2021@gmail.com>
2025-01-10 19:41:52 +0530

    Time: 6 ms (27.08%) | Memory: 46 MB (21.54%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -12,14 +12,19 @@ public:
         int i = 0; // Start index of the current window
 
         for (int j = 1; j < n; j++) {
-            int cmp = (arr[j - 1] < arr[j]) ? 1 : (arr[j - 1] > arr[j] ? -1 : 0);
+            // find the cmp for j and j-1
 
-            if (cmp == 0) {
-                // Reset the start of the turbulent subarray
+            int prevcmp = (arr[j - 1] < arr[j]) ? 1 : (arr[j - 1] > arr[j] ? -1 : 0);
+            
+            int nextcmp = 0;
+
+            if(j<n-1) nextcmp = (arr[j] < arr[j+1]) ? 1 : (arr[j] > arr[j+1] ? -1 : 0);
+
+            if (prevcmp == 0) {
+                // Reset the start of the turbulent subarray as the elements are equal to one another
                 i = j;
             } 
-            
-            else if (j == n - 1 || cmp * ((arr[j] < arr[j + 1]) ? 1 : (arr[j] > arr[j + 1] ? -1 : 0)) != -1) {
+            else if (j == n - 1 || prevcmp*nextcmp != -1) {
                 // If the current sequence breaks or we reach the end, update the answer
                 ans = max(ans, j - i + 1);
                 i = j;
