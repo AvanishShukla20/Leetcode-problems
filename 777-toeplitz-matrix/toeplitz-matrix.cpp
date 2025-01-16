2c7bb2467a6ecae8040e07b82872ce4f28194bc2
Avanish Shukla <avanishshukla2021@gmail.com>
2025-01-16 23:58:02 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (52.30%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.cpp
+++ 777-toeplitz-matrix/toeplitz-matrix.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    bool mark(int i, int j, vector<vector<int>>& matrix)
-    {
-        int m=matrix.size(), n= matrix[0].size();
-        int val = matrix[i][j];
-        while(i < m && j < n)
-        {
-            if(matrix[i][j] != val) return false;
-            matrix[i][j] = -1;
-            i++;
-            j++;
-        }
-        return true;
-    }
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         int m=matrix.size(), n= matrix[0].size();
 
-        for(int i=0; i<m; i++)
+        for(int i=1; i<m; i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j=1; j<n; j++)
             {
-                if(matrix[i][j] != -1)
+                if(matrix[i][j] != matrix[i-1][j-1])
                 {
-                    bool x = mark(i, j, matrix);
-                    if(x == false) return false;
+                    return false;
                 }
                 
             }
