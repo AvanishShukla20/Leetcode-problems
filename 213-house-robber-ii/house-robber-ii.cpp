d8281baebb6f3c14adcec0204a68a74fb68910a2
Avanish Shukla <avanishshukla2021@gmail.com>
2025-01-21 11:12:16 +0530

    Time: 6 ms (1.07%) | Memory: 12.3 MB (5.79%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    int solve(int i, int prev, bool& p, vector<int>& nums, vector<vector<vector<int>>>& dp)
+    int solve(int i, int prev,int last_idx, vector<int>& nums, vector<vector<int>>& dp)
     {
-        if(i == nums.size())
+        if(i >= last_idx)
         {
             return 0;
         }
 
-        if(dp[i][prev+1][p] != -1) return dp[i][prev+1][p];
+        if(dp[i][prev+1] != -1) return dp[i][prev+1];
 
         int left = 0, right = 0;
         if(prev == -1 || i > prev+1)
         {
-            if(i == 0)
-            {
-                p = true;
-                left = nums[i] + solve(i+1, i, p, nums, dp);
-                p = false;
-            }
-            else if(i == nums.size()-1 && p == true)
-            {
-                left = solve(i+1, prev, p, nums, dp);
-            }
-            else left = nums[i] + solve(i+1, i, p, nums, dp);    
+            left = nums[i] + solve(i+1, i, last_idx, nums, dp);    
         }
 
 
-        right = solve(i+1, prev, p, nums, dp);
-        return dp[i][prev+1][p] = max(left, right);
+        right = solve(i+1, prev, last_idx, nums, dp);
+        return dp[i][prev+1] = max(left, right);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        bool p = false;
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n+1, vector<int> (2, -1)));
-
-        return solve(0, -1, p, nums, dp);
+        if(n==1) return nums[0];
+        
+        vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        int ans1 = solve(0, -1, n-1, nums, dp);
+        vector<vector<int>> dp2(n, vector<int>(n+1, -1));
+        int ans2 = solve(1, -1, n, nums, dp2);
+        return max(ans1, ans2);
     }
 };
