47c0ce7c096357ed4a271538dc1cb344cd1c3896
Avanish Shukla <avanishshukla2021@gmail.com>
2025-01-21 15:52:07 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (88.75%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int, int> solve(TreeNode* root)
+    {
+        if(!root)
+        {
+            return {0, 0};
+        }
+        pair<int, int> leftPair  = solve(root->left);
+        pair<int, int> rightPair = solve(root->right);
+
+        //including the root i will add values gotten by not including the children
+        int lsum = root->val + leftPair.second + rightPair.second;
+        // not including the root
+        int rsum = max(leftPair.first, leftPair.second) + max(rightPair.first, rightPair.second);
+
+        return {lsum, rsum};
+
+    }
+    int rob(TreeNode* root) {
+        pair<int, int> ans = solve(root);
+        
+        return max(ans.first, ans.second);
+    }
+};
