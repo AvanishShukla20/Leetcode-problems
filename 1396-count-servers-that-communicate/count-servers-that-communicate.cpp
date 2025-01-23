b0bcd1e54766f8a6d7bff47fafc26fcc5a272e10
Avanish Shukla <avanishshukla2021@gmail.com>
2025-01-23 20:53:33 +0530

    Time: 4 ms (44.09%) | Memory: 26.6 MB (73.49%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
-        int numRows = grid.size(), numCols = grid[0].size();
-        int communicableServersCount = 0;
+        int m=grid.size(), n=grid[0].size();
 
-        // Traverse through the grid
-        for (int row = 0; row < numRows; ++row) {
-            for (int col = 0; col < numCols; ++col) {
-                if (grid[row][col] == 1) {
-                    bool canCommunicate = false;
+        vector<int> RowCnt(m, 0);
+        vector<int> ColCnt(n, 0);
 
-                    // Check for communication in the same row
-                    for (int otherCol = 0; otherCol < numCols; ++otherCol) {
-                        if (otherCol != col && grid[row][otherCol] == 1) {
-                            canCommunicate = true;
-                            break;
-                        }
-                    }
-
-                    // If a server was found in the same row, increment
-                    // communicableServersCount
-                    if (canCommunicate) {
-                        communicableServersCount++;
-                    } else {
-                        // Check for communication in the same column
-                        for (int otherRow = 0; otherRow < numRows; ++otherRow) {
-                            if (otherRow != row && grid[otherRow][col] == 1) {
-                                canCommunicate = true;
-                                break;
-                            }
-                        }
-
-                        // If a server was found in the same column, increment
-                        // communicableServersCount
-                        if (canCommunicate) {
-                            communicableServersCount++;
-                        }
-                    }
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(grid[i][j] == 1)
+                {
+                    RowCnt[i] += 1;
+                    ColCnt[j] += 1;
                 }
             }
         }
 
-        return communicableServersCount;
+        int cnt =0;
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                
+                if(grid[i][j] == 1)
+                {
+                    //presence of any single server other than current akes this current as communicable
+                    // in row or in col
+                    if(RowCnt[i] - 1 > 0 || ColCnt[j] - 1 > 0) cnt++;
+                    
+                }
+            }
+        }
+
+        return cnt;
     }
 };
