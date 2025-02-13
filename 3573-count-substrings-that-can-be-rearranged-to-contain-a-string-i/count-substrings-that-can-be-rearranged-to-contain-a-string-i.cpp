a101d2ce4ba2f8f489720a47a1f425f75b9a7deb
Avanish Shukla <avanishshukla2021@gmail.com>
2025-02-14 03:07:34 +0530

    Time: 132 ms (18.73%) | Memory: 18.3 MB (15.44%) - LeetSync

--- 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
+++ 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    bool isValid(unordered_map<char, int>& reqmpp, unordered_map<char, int>& currmpp) {
-        for (auto& it : reqmpp) {
-            if (currmpp[it.first] < it.second) 
-                return false;
+    bool isvalid(unordered_map<char, int>& reqmpp, unordered_map<char, int>& currmpp)
+    {
+        for(auto &it : reqmpp)
+        {
+            if(currmpp[it.first] < it.second) return false;
         }
         return true;
     }
-
     long long validSubstringCount(string word1, string word2) {
         unordered_map<char, int> currmpp, reqmpp;
-        int n = word1.size();
-        long long ans = 0;
-        
-        // Populate frequency map for word2
-        for (char ch : word2) {
-            reqmpp[ch]++;
+        int n=word1.size(), m= word2.size();
+
+        for(int i=0; i<word2.size(); i++)
+        {
+            reqmpp[word2[i]]++;
         }
 
-        int i = 0;  // Left pointer for sliding window
+        long long ans = 0, i = 0;
+        for(int j=0; j<n; j++)
+        {
+            char curr = word1[j];
+            currmpp[curr]++;
 
-        for (int j = 0; j < n; j++) {
-            currmpp[word1[j]]++;  // Expand the window
-
-            // Shrink the window from the left while it's valid
-            while (isValid(reqmpp, currmpp)) {
-                ans += (n - j);  // Count all substrings starting at i
-                currmpp[word1[i]]--;  // Remove leftmost character
-                if (currmpp[word1[i]] == 0) currmpp.erase(word1[i]);  // Cleanup
-                i++;  // Move left pointer
+            while(isvalid(reqmpp, currmpp) == true)
+            {
+                ans += (n-j);
+                currmpp[word1[i]]--;
+                if(currmpp[word1[i]] == 0) currmpp.erase(word1[i]);
+                i++;
             }
         }
 
