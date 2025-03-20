ec481db66c2be24e5b54201ad3d853c257472a18
Avanish Shukla <avanishshukla2021@gmail.com>
2025-03-20 19:58:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (34.86%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // wherever you feel there is need of conversion of row into col OR VICE-VERSA apply TRANSPOSE OF MATRIX there
-        int m = matrix.size();
-        int n = m;
-        for(int i = 0; i < m ; i++)
+        int m=matrix.size(), n=matrix[0].size();
+        for(int i=0; i<m; i++)
         {
-            for(int j = i + 1 ; j < n; j++)
+            for(int j=i; j<n; j++)
             {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        for(int i = 0; i<m; i++)
+        for(int i=0; i<m; i++)
         {
             reverse(matrix[i].begin(), matrix[i].end());
         }
-
         
-
-
 
     }
 };
