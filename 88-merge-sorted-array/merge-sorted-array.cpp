00cf5e697a3f3532b8261cd66d4eed785dbe9326
Avanish Shukla <avanishshukla2021@gmail.com>
2025-03-21 20:40:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (95.78%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        int j = n - 1;
-        int i = m - 1;
-        int k = m + n - 1;
+        // consider testcase To bulid solution -> n1 = [1, 2, 4,6, 8, 10, 12, 0, 0, 0, 0] and 
+        // n2 => [5, 6,7, 9, 13] and realise -> .........Going backwards is a good option
+
+        // edge case-> n1 = [3, 4, 0] n2 => [1]
+
+        int i=m-1, j=n-1, k=m+n-1;
 
         while(j >= 0)
         {
@@ -18,8 +20,8 @@ public:
                 nums1[k] = nums2[j];
                 j--;
             }
-
             k--;
         }
+
     }
 };
