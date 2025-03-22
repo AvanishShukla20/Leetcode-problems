3947ee9e55b305c057b2028e6b4d815d0ff0811a
Avanish Shukla <avanishshukla2021@gmail.com>
2025-03-22 19:44:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (45.03%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    double calculateRes(double x, int n)
+    double solve(double x, int n)
     {
-        if(n==0)
-        {
-            return 1;
-        }
-        double res = myPow(x, n/2);
+        if(n == 0) return 1;
 
-        res *= res;
-        if(n%2 !=0) res *= x;
+        double res = 1;
+        double p = solve(x, n/2);
+        
+        res = p*p;
 
+        if(n%2 != 0) res *= x;
         return res;
     }
     double myPow(double x, int n) {
-        double ans = calculateRes(x,abs(n));
-        if(n<0) return 1.00/ans;
-        else return ans;
+
+        double ans = solve(x, n);
+        if(n < 0) ans = 1/ans;
+        return ans;
     }
 };
