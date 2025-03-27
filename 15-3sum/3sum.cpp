7aadedca9592a2026dfa7ca2e460f9552fe5e6e7
Avanish Shukla <avanishshukla2021@gmail.com>
2025-03-27 23:16:24 +0530

    Time: 44 ms (81.33%) | Memory: 29.2 MB (44.19%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,39 +4,50 @@ public:
         int n=nums.size();
 
         sort(nums.begin(), nums.end());
+
+
         vector<vector<int>> ans;
-        set<vector<int>> ansSet;
-
-
-        for(int i = 0; i<n; i++)
+        int prev_i_val = 0;
+        for(int i=0; i<n; i++)
         {
+            if(i > 0 && prev_i_val == nums[i]) continue;
 
-            //using two pointers and total sum algorithm to get sum = 0
-            int j = i+1;
-            int k = n-1; // the two pointers we will be using are (j, k)
-
-            while(j<n && j < k)
+            int start = i+1;
+            int end = n-1;
+            prev_i_val = nums[i];
+            
+            while(start < end)
             {
-                int sum = nums[i]+nums[j]+nums[k];
-
+                int sum = nums[start] + nums[end] + nums[i];
                 if(sum > 0)
                 {
-                    k--;
+                    end--;
                 }
-                else if(sum < 0) j++;
+                else if(sum < 0) start++;
                 else
                 {
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
-                    sort(temp.begin(), temp.end());
-                    ansSet.insert(temp);
-                    k--;
-                    j++;
+                    ans.push_back({nums[i], nums[start], nums[end]});
+                    // increment start and decrement end until you find a different value at nums[start] and nums[end] respectively
+
+                    int v_start = nums[start];
+                    while(start < n && nums[start] == v_start)
+                    {
+                        start++;
+                    }
+
+                    int v_end = nums[end];
+
+                    while(end >= 0 && nums[end] == v_end)
+                    {
+                        end--;
+                    }
+
+                    if(start >= end) break;
                 }
             }
             
         }
 
-        for(auto &it : ansSet) ans.push_back(it);
         return ans;
     }
 };
