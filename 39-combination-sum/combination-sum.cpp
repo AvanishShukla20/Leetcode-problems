ee4abe561562e5ec614eb3c6e13b9f148bc7a937
Avanish Shukla <avanishshukla2021@gmail.com>
2025-03-31 22:53:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.33%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
-    /* Since this ques tells us that candidates is distinct That's why there is no chance of having a DUPLICATE valid Combination in our approach */
-
-    vector<vector<int>> ans;
-    void findValidCombinations(vector<int>& candidates, int target, int idx, vector<int>& validcandidate)
+    void solve(int idx, vector<int>& nums, int target, vector<int>& temp, vector<vector<int>>& ans)
     {
-
-        if(idx == candidates.size())
+        if(target == 0)
         {
-            if(target == 0)
-            {
-                ans.push_back(validcandidate);
-                // we have not done pop_back here as this call will always be returned as a nonpick condition
-            }
+            ans.push_back(temp);
             return;
         }
-        
+        if(idx < 0) return;
 
-        if(target - candidates[idx] >= 0)
+        if(target >= nums[idx])
         {
-            validcandidate.push_back(candidates[idx]);
-            cout<<target<<" ";
-            findValidCombinations(candidates, target - candidates[idx], idx,validcandidate);
-            cout<<target<<endl;
-            validcandidate.pop_back();
+            temp.push_back(nums[idx]);
+            solve(idx, nums, target-nums[idx], temp, ans);
+            temp.pop_back();
         }
 
+        solve(idx-1, nums, target, temp, ans);
 
-        // Why i am not doing any check of target below as i have done before ? ->
-        /* It is because this step does not affects the target at all */
-
-        /*NON-PICK*/ findValidCombinations(candidates, target , idx+1, validcandidate);
     }
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        if(candidates.size() == 0) return {};
-        vector<int> validcandidate;
-        int idx = 0;
-        findValidCombinations(candidates, target, idx, validcandidate);
+        int n=candidates.size();
+        int sum = 0;
+        vector<int> temp;
+        vector<vector<int>> ans; 
+        solve(n-1, candidates, target, temp, ans);
         return ans;
     }
 };
