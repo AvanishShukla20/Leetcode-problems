3187ad369435c5fdbc3ba3ef7c7c5cbc8dcd6470
Avanish Shukla <avanishshukla2021@gmail.com>
2025-03-31 16:57:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (52.52%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     void solve(int idx, vector<int>& temp, vector<vector<int>>& ans, vector<int>& nums)
     {
-        // Ques -> 2 2 2 3
+        // Ques -> 1 2 2 2 3 3
         if(idx == nums.size())
         {
             ans.push_back(temp);
             return;
         }
-        // use 2 and find for 2 2 3
+        
         temp.push_back(nums[idx]);
         solve(idx+1, temp, ans, nums);
-        // remove 2
+        
         temp.pop_back();
 
-        //increment idx until you get different element
+        //increment idx until you get different value for this size subset
         while(idx < nums.size() - 1 && nums[idx] == nums[idx+1]) idx++;
 
-        // 3 is different so call for it
+       
         solve(idx + 1,temp, ans, nums);
 
     }
