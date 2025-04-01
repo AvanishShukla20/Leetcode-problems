7ac72cbc3f179203a05b8974f5f7e275bf3cf406
Avanish Shukla <avanishshukla2021@gmail.com>
2025-04-01 23:48:37 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.54%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -9,19 +9,27 @@ public:
         while(low <= high)
         {
             mid = low+(high-low)/2;
-            checkidx = mid;
-
-            if(mid+1 < n && nums[mid] == nums[mid+1]) checkidx++;
-            if((mid-1 >= 0 && nums[mid] != nums[mid-1]) &&  (mid+1 < n && nums[mid] !=  nums[mid+1]))
+            if(mid == 0)
             {
-                return nums[mid];
-            } 
-
-            if(checkidx%2 != 0)
-            {
-                low = mid+1;
+                if(nums[mid] != nums[mid+1]) return nums[mid];
+                else low = mid+1;
             }
-            else high = mid-1;
+            else if(mid == n-1)
+            {
+                if(nums[mid] != nums[mid-1]) return nums[mid];
+                else high = mid-1;
+            }
+            else
+            {
+                if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]) return nums[mid];
+
+                int id1 = mid, id2 = mid;
+                if(nums[mid] == nums[mid+1]) id2++;
+                else if(nums[mid] == nums[mid-1]) id1--;
+
+                if(id1%2 != 0 && id2%2 == 0) high = mid-1;
+                else low = mid+1;
+            }
         }
 
         return nums[low];
